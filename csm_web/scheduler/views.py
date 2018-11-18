c0077889e3840a4cc53b08fe28e8bd03bff91d68
Matthew Soh <matthewsoh.cw@gmail.com>
2018-11-18 14:08:10 -0800

    Add Google OAuth2 login/logout

--- csm_web/scheduler/views.py
+++ csm_web/scheduler/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import logout as auth_logout
 
-# Create your views here.
+def login(request):
+    return render(request, 'scheduler/login.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect(reverse('index'))
+
+def index(request):
+    return render(request, 'scheduler/index.html', {'user': request.user})
