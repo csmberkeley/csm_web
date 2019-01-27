b9fa0932430a5339b586acc24428ea0493e2c34c
Matthew Soh <matthewsoh.cw@gmail.com>
2019-01-26 22:25:25 -0800

    Add day of week accordion

--- csm_web/frontend/src/components/Course.js
+++ csm_web/frontend/src/components/Course.js
@@ -1,18 +1,86 @@
 import React from "react";
+import { groupBy } from "lodash";
+
+const dayOfWeek = {
+  Monday: 0,
+  Tuesday: 1,
+  Wednesday: 2,
+  Thursday: 3,
+  Friday: 4,
+  Saturday: 5,
+  Sunday: 6
+};
 
 function CourseDetail(props) {
   return <h1>{props.course}</h1>;
 }
 
-function Course(props) {
-  return (
-    <div>
-      <CourseDetail course={props.course} />
-    </div>
-  );
+class Course extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: props.course,
+      sections: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch(`/scheduler/courses/${this.state.course}/sections/`)
+      .then(response => response.json())
+      .then(sections =>
+        this.setState((state, props) => {
+          return {
+            sections: groupBy(
+              sections,
+              section => section.defaultSpacetime.dayOfWeek
+            )
+          };
+        })
+      );
+  }
+
+  render() {
+    const days = Object.entries(this.state.sections)
+      .sort((item1, item2) => {
+        const day1 = dayOfWeek[item1[0]];
+        const day2 = dayOfWeek[item2[0]];
+        if (day1 == day2) {
+          return 0;
+        } else if (day1 < day2) {
+          return 1;
+        } else {
+          return -1;
+        }
+      })
+      .reverse()
+      .map(item => {
+        const [day, sections] = item;
+        return <Day key={day} day={day} sections={sections} />;
+      });
+
+    return (
+      <div>
+        <div>
+          <CourseDetail course={this.state.course} />
+        </div>
+        <div>
+          <ul uk-accordion="true">{days}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
-function Day(props) {}
+function Day(props) {
+  return (
+    <li>
+      <a className="uk-accordion-title" href="#">
+        {props.day}
+      </a>
+      <div className="uk-accordion-content">Lorem ipsum dolor sit amet</div>
+    </li>
+  );
+}
 
 function SectionSummary(props) {}
 
