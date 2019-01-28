5105d39d12398432fc20198dd306eb40df382b73
Matthew Soh <matthewsoh.cw@gmail.com>
2019-01-27 21:24:04 -0800

    Add sanity/security check to post

--- csm_web/frontend/src/utils/api.js
+++ csm_web/frontend/src/utils/api.js
@@ -1,6 +1,11 @@
 import Cookies from "js-cookie";
 
 export function post(endpoint, data) {
+  const relative_endpoint_pattern = RegExp("^([A-z0-9]+/)+$");
+  if (!relative_endpoint_pattern.test(endpoint)) {
+    throw new Error("post should only be used with relative endpoints");
+  }
+
   return fetch(endpoint, {
     method: "POST",
     credentials: "same-origin",
