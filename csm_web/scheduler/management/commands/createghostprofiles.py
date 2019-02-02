a9a66dcfe9d31c998a57bbf799930ae4c99a4a1e
noloerino <jhshi07@gmail.com>
2019-02-01 22:32:22 -0800

    Add ability to pass CSV with headers to ghost profiles

--- csm_web/scheduler/management/commands/createghostprofiles.py
+++ csm_web/scheduler/management/commands/createghostprofiles.py
@@ -6,62 +6,104 @@ from scheduler.models import Profile, Course, User
 # from django.contrib.auth.models import User as AuthUser
 import csv
 
-PROFILE_CSV_DEFAULT_FIELDS = ("email", "course", "role")
-PROFILE_CSV_DEFAULT_ACTIONS = {
-    "email": lambda s: s,
-    "course": lambda s: Course.objects.get(name=s),
-    "role": lambda s: s,
+PROFILE_CSV_DEFAULT_FIELDS = ("name", "email", "role")
+
+ROLE_MAP = {
+    "Coordinator": Profile.COORDINATOR,
+    "Senior Mentor": Profile.SENIOR_MENTOR,
+    "Junior Mentor": Profile.JUNIOR_MENTOR,
+    "Student": Profile.STUDENT,
 }
 
 
 class Command(BaseCommand):
     help = """Creates 'ghost' profiles for users who have not yet logged in to Scheduler.
-	This takes in a CSV file where each row contains the user's name, email, and role, and generates
-	profile objects as specified.
-	The path to the CSV file and the course the file is for are specified as arguments.
-	User profiles are created through the Django ORM, and the social/oauth objects will be associated
-	for those users when they log in for the first time.
-	"""
+    This takes in a CSV file where each row contains the user's name, email, and role, and generates
+    profile objects as specified.
+    The path to the CSV file and the course the file is for are specified as arguments.
+    User profiles are created through the Django ORM, and the social/oauth objects will be associated
+    for those users when they log in for the first time.
     """
-	Refer here for details on associating python-social-auth objects with users:
-	https://python-social-auth-docs.readthedocs.io/en/latest/configuration/django.html#personalized-configuration
-	"""
+    """
+    Refer here for details on associating python-social-auth objects with users:
+    https://python-social-auth-docs.readthedocs.io/en/latest/configuration/django.html#personalized-configuration
+    """
 
     def add_arguments(self, parser):
-        parser.add_argument("csv_path", nargs=1, type=str)
-        parser.add_argument("--student", action="store_true", dest="is_students")
-        parser.add_argument("--course", action="store", dest="course", type=str)
+        parser.add_argument(
+            "csv_path", type=str, help="the path to the CSV file to be read"
+        )
+        parser.add_argument(
+            "course", type=str, help="the slug for the course being entered"
+        )
+        parser.add_argument(
+            "--student",
+            action="store_true",
+            dest="is_students",
+            help="if specified, assumes that all users entered are students",
+        )
+        parser.add_argument(
+            "--withheader",
+            action="store_true",
+            dest="withheader",
+            help="specifies that the first row of the CSV file should be read as column names, otherwise uses PROFILE_CSV_DEFAULT_FIELDS",
+        )
 
     def handle(self, *args, **options):
-        filename = options["csv_path"][0]
+        filename = options["csv_path"]
+        count = 0
         try:
             with open(filename) as csvfile:
+                reader = iter(csv.reader(csvfile))
+                if options["withheader"]:
+                    # treat the first row as column names
+                    cols = [s.lower() for s in next(reader)]
+                else:
+                    cols = PROFILE_CSV_DEFAULT_FIELDS
+                self._check_cols(cols, options)
+                course = Course.objects.get(name=options["course"])
                 try:
                     with transaction.atomic():
-                        reader = csv.reader(csvfile)
                         for row in reader:
-                            self._create_single_profile(row, options)
+                            self._create_single_profile(cols, row, course, options)
+                            count += 1
                 except IntegrityError as e:
-                    self.stderr.write("Error while generating ghost profiles")
-                    self.stderr.write(e)
-        except csv.Error:
+                    self.stderr.write(
+                        "Failed after generating {} profile(s)".format(count)
+                    )
+                    raise e
+        except csv.Error as e:
             self.stderr.write("Unable to read CSV file.")
+            raise e
+        self.stdout.write("Generated {} profile(s).".format(count))
 
-    def _create_single_profile(self, row, options):
-        """Creates a single profile object from the parameters passed in the row of a CSV file."""
+    def _check_cols(self, cols, options):
+        if options["is_students"]:
+            if "role" in cols:
+                raise Exception(
+                    "'role' column was found in CSV, but --student was specified"
+                )
+        elif "role" not in cols:
+            raise Exception("'role' column missing (found {})".format(cols))
+        if "email" not in cols:
+            raise Exception("'email' column missing (found {})".format(cols))
+
+    def _create_single_profile(self, cols, row, course, options):
+        """
+        Creates a single profile object from the parameters passed in the row of a CSV file.
+        - cols: an iterable specifying the order of the columns
+        - row: the row of the CSV file (a list of values)
+        """
         # TODO for students, match section to mentor, and generate attendances (maybe as an object hook?)
         fields = {}
         for i in range(len(row)):
-            field = PROFILE_CSV_DEFAULT_FIELDS[i]
-            fields[field] = PROFILE_CSV_DEFAULT_ACTIONS[field](row[i])
+            field = cols[i]
+            fields[field] = row[i]
         if options["is_students"]:
             fields["role"] = Profile.STUDENT
-        if options["course"] and options["course"] != fields["course"]:
-            raise Exception(
-                "Course in CSV does not match course provided as argument"
-            )
-            # NOTE: unsure if this accounts for normalization, i.e. fname.lname@gmail vs fnamelname2gmail
-            # also unsure if lack of password will mess with social-auth; that's to be tested
+        # NOTE: the "name" field is ignored, as we're letting oauth handle that for us
+        # NOTE: unsure if this accounts for normalization, i.e. fname.lname@gmail vs fnamelname@gmail
+        # also unsure if lack of password will mess with social-auth; that's to be tested
         email = fields["email"]
         user, _ = User.objects.get_or_create(username=email.split("@")[0], email=email)
-        Profile.objects.create(role=fields["role"], course=fields["course"], user=user)
+        Profile.objects.create(role=fields["role"], course=course, user=user)
--- csm_web/scheduler/tests/test_ghost_profile_maker.py
+++ csm_web/scheduler/tests/test_ghost_profile_maker.py
@@ -1,8 +1,7 @@
-from io import StringIO
 import csv
+import os
 from django.core.management import call_command
 from django.test import TestCase
-from tempfile import NamedTemporaryFile
 import scheduler.factories as factories
 from scheduler.models import User, Profile, Course
 from scheduler.management.commands.createghostprofiles import (
@@ -21,19 +20,12 @@ class TestGhostProfileMaker(TestCase):
     @classmethod
     def setUpTestData(cls):
         c = factories.CourseFactory.create(name=cls.TEST_COURSE)
-        mentor = Profile.objects.create(
-            leader=None,
-            course=c,
-            role=Profile.JUNIOR_MENTOR,
-            user=factories.UserFactory.create(),
-            section=None,
-        )
         # not using ProfileFactory, because of some weird recursion depth exceeded error
         profiles = []
         for i in range(cls.NUM_STUDENTS):
             profiles.append(
                 Profile(
-                    leader=mentor,
+                    leader=None,
                     course=c,
                     role=Profile.STUDENT,
                     user=factories.UserFactory.create(),
@@ -41,38 +33,62 @@ class TestGhostProfileMaker(TestCase):
                 )
             )
         profile_dicts = []
-        f = NamedTemporaryFile(mode="w", prefix="good_profiles", suffix="csv")
-        w = csv.writer(f)
-        for profile in profiles:
-            # avoid saving to db with build instead of create
-            profile_dict = {
-                "email": profile.user.email,
-                "course": c,
-                "role": profile.role,
-            }
-            profile_dicts.append(profile_dict)
-            line = [
-                e for e in map(profile_dict.get, PROFILE_FIELD_ORDER) if e is not None
-            ]
-            w.writerow(line)
-        cls.good = {"filename": f.name, "profile_dicts": profile_dicts}
-        cls.f = f
+
+        def make_test_csvs(filenames, headers):
+            assert len(filenames) == len(headers)
+            fs = [open(filename, "x") for filename in filenames]
+            ws = [csv.writer(f) for f in fs]
+            for i in range(len(ws)):
+                if headers[i]:
+                    ws[i].writerow(headers[i])
+            for profile in profiles:
+                # avoid saving to db with build instead of create
+                profile_dict = {
+                    "name": profile.user.get_full_name(),
+                    "email": profile.user.email,
+                    "role": profile.role,
+                }
+                profile_dicts.append(profile_dict)
+                line = [
+                    e
+                    for e in map(profile_dict.get, PROFILE_FIELD_ORDER)
+                    if e is not None
+                ]
+                [w.writerow(line) for w in ws]
+            [f.close() for f in fs]
+
+        no_header = "no_header_profiles.csv"
+        with_header = "with_header_profiles.csv"
+        make_test_csvs([no_header, with_header], [(), PROFILE_FIELD_ORDER])
+        cls.profile_dicts = profile_dicts
+        cls.no_header_filename = no_header
+        cls.with_header_filename = with_header
+        cls._f = [open(no_header, "r"), open(with_header, "r")]
 
     @classmethod
     def tearDownClass(cls):
-        cls.f.close()
+        [f.close() for f in cls._f]
+        [os.remove(f.name) for f in cls._f]
 
     def test_success_command_output(self):
         course = self.TEST_COURSE
-        call_command("createghostprofiles", self.good["filename"])
-        missing = []
-        for profile_dict in self.good["profile_dicts"]:
-            # check to ensure that the users all exist
-            user = User.objects.get(email=profile_dict["email"])
-            # assume only one profile per user created
-            try:
+        self.assertEqual(course, Course.objects.all().first().name)
+
+        def check():
+            for profile_dict in self.profile_dicts:
+                # check to ensure that the users all exist
+                user = User.objects.get(email=profile_dict["email"])
+                # assume only one profile per user created
                 profile = Profile.objects.get(user=user)
                 self.assertEqual(course, profile.course.name)
                 self.assertEqual(profile_dict["role"], profile.role)
-            except Profile.DoesNotExist:
-                missing += list(profile_dict.values())
+            self.assertEqual(self.NUM_STUDENTS, Profile.objects.count())
+
+        call_command("createghostprofiles", self.no_header_filename, course)
+        check()
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        call_command(
+            "createghostprofiles", self.with_header_filename, course, "--withheader"
+        )
+        check()
