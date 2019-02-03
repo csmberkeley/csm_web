fa7df5bdd0617f211384e1c31ca17b133f922ade
Matthew Soh <msohcw@gmail.com>
2019-02-04 02:33:57 +0800

    Style course list

--- csm_web/frontend/src/components/CourseNav.js
+++ csm_web/frontend/src/components/CourseNav.js
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 function CourseNav(props) {
   const courseListEntries = Object.keys(props.courses).map(course => (
-    <li key={course}>
+    <li className="course-nav-entry" key={course}>
       <Link to={`/courses/${course}`}>
-        <h4>{course.toUpperCase()}</h4>
+        <h4 className="course-nav-entry-text">{course.toUpperCase()}</h4>
       </Link>
     </li>
   ));
-  return <ul class="uk-list uk-list-divider">{courseListEntries}</ul>;
+  return <ul class="uk-list uk-list-striped">{courseListEntries}</ul>;
 }
 
 export default CourseNav;
