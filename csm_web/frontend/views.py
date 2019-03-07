7c89923157fa143b431e8c2a045d94da3fe2d6f7
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-03-06 17:53:07 -0800

    Conditionally use React production/development build based on environment

--- csm_web/frontend/views.py
+++ csm_web/frontend/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
 
 @login_required
 def index(request):
-    return render(request, "frontend/index.html")
+    return render(
+        request, "frontend/index.html", context={"DJANGO_ENV": settings.DJANGO_ENV}
+    )
