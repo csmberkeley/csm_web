5e4025dbb8107dc960bb60b31d256ca7825551be
noloerino <jhshi07@gmail.com>
2019-03-10 17:30:29 -0700

    Add attendance test cases

--- csm_web/csm_web/settings.py
+++ csm_web/csm_web/settings.py
@@ -193,6 +193,7 @@ if DEBUG:
 # REST Framework
 REST_FRAMEWORK = {"DEFAULT_RENDERER_CLASSES": REST_RENDERERS}
 
+<<<<<<< HEAD
 if DJANGO_ENV == DEVELOPMENT:
     REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"].append(
         "rest_framework.renderers.BrowsableAPIRenderer"
