8904eb08b17b5eee6f06ebfff032341c4ceb9c2c
Dhruv Jhamb <dhruvsaijhamb@gmail.com>
2019-04-04 21:09:40 -0700

    Added Matching model and endpoints

--- csm_web/logistinator/models.py
+++ csm_web/logistinator/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ActivatableModel(models.Model):
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Matching(ActivatableModel):
+    ROOM_CHOICES = (
+        ("Cory-144MB", "Cory-144MB"),
+        ("Cory-258", "Cory-258"),
+        ("Cory-258", "Cory-258"),
+        ("Cory-400", "Cory-400"),
+        ("Cory-504", "Cory-504"),
+        ("Cory-521", "Cory-521"),
+        ("Cory-531", "Cory-531"),
+        ("Cory-540AB", "Cory-540AB"),
+        ("Cory-557", "Cory-557"),
+        ("Cory-Classroom-293", "Cory-Classroom-293"),
+        ("Cory-Classroom-299", "Cory-Classroom-299"),
+        ("Soda-306", "Soda-306"),
+        ("Soda-310", "Soda-310"),
+        ("Soda-320", "Soda-320"),
+        ("Soda-373", "Soda-373"),
+        ("Soda-380", "Soda-380"),
+        ("Soda-405", "Soda-405"),
+        ("Soda-430-438", "Soda-430-438"),
+        ("Soda-511", "Soda-511"),
+        ("Soda-606", "Soda-606"),
+        ("Soda-Alcove-283E", "Soda-Alcove-283E"),
+        ("Soda-Alcove-283H", "Soda-Alcove-283H"),
+        ("Soda-Alcove-341A", "Soda-Alcove-341A"),
+        ("Soda-Alcove-341B", "Soda-Alcove-341B"),
+        ("Soda-Alcove-411", "Soda-Alcove-411"),
+        ("Soda-Alcove-611", "Soda-Alcove-611"),
+        ("Dummy", "Dummy"),
+    )
+
+    user_id = models.CharField(max_length=100)
+    room_id = models.CharField(max_length=16, choices=ROOM_CHOICES, blank=True)
+    start_datetime = models.DateField()
+    end_datetime = models.DateField()
+    weekly = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "({person}, {room}, {start}, {end}, {weekly})".format(
+            person=self.user_id,
+            room=self.room_id,
+            start=self.start_datetime,
+            end=self.end_datetime,
+            weekly="Yes" if self.weekly else "No",
+        )
--- csm_web/logistinator/views.py
+++ csm_web/logistinator/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from django.db import transaction
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth import logout as auth_logout
+
+from rest_framework import generics, permissions, viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+
+from .models import Matching
+from .serializers import (
+    MatchingSerializer
+)
+from .permissions import (
+    is_leader,
+    IsLeader,
+    IsLeaderOrReadOnly,
+    IsReadIfOwner,
+    IsOwner,
+    DestroyIsOwner,
+)
 
 # Create your views here.
+
+# Matching
+
+@api_view(http_method_names=["POST"])
+def update(request, pk):
+
+    matching = get_object_or_404(Flag, pk=pk)
+
+    dic = request.POST
+
+    matching.user_id = dic["user_id"]
+    matching.room_id = dic["room_id"]
+    matching.start_datetime = dic["start_datetime"]
+    matching.end_datetime = dic["end_datetime"]
+    matching.weekly = dic["weekly"]
+
+    matching.save()
+
+    serialized_matching = MatchingSerializer(matching).data
+    return Response(serialized_matching)
+
+@api_view(http_method_names=["POST"])
+def get_by_user(request):
+
+    dic = request.POST
+
+    serialized_matching = MatchingSerializer(Matching.objects.filter(user_id=dic["user_id"])).data
+    return Response(serialized_matching)
+
+@api_view(http_method_names=["POST"])
+def get_by_room(request):
+    
+	dic = request.POST
+
+	serialized_matching = MatchingSerializer(Matching.objects.filter(room_id=dic["room_id"])).data
+    return Response(serialized_matching)
+
+# REST Framework API Views
+
+class CreateMatching(generics.CreateAPIView):
+
+    queryset = Matching.objects.all().order_by("-date_joined")
+    serializer_class = MatchingSerializer
+
+    def create(self, request, *args, **kwargs):
+	    return Response(status=204)
+
+class DeleteMatching(generics.DestroyAPIView):
+
+	permission_classes = (DestroyIsOwner,)
+
+    def destroy(self, request, *args, **kwargs):
+        matching = get_object_or_404(Matching, pk=self.kwargs["pk"])
+        self.check_object_permissions(request, matching)
+        if not matching.active:
+            raise PermissionDenied(
+                "This matching ({}) has been deactivated".format(matching)
+            )
+        matching.active = False
+        matching.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
