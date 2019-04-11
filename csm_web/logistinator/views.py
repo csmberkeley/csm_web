3776aa2b2244d690ccfc914fc9204eb59d96ebe6
Matthew Soh <matthewsoh@berkeley.edu>
2019-04-10 19:49:29 -0700

    Add stubbed Availability endpoint

--- csm_web/logistinator/views.py
+++ csm_web/logistinator/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Availability
+from .serializers import AvailabilitySerializer
 
-# Create your views here.
+# Stubbed Availabiity Model
+class AvailabilityViewSet(viewsets.ModelViewSet):
+    queryset = Availability.objects.all()
+    serializer_class = AvailabilitySerializer
