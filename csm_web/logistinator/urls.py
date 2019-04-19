150e15a2ce067e868f0b71c68157a5b0a95cd85c
MrunalPuram <mrunal.puram@gmail.com>
2019-04-18 21:32:33 -0700

    Added endpoints for Availability

--- csm_web/logistinator/urls.py
+++ csm_web/logistinator/urls.py
@@ -2,6 +2,34 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import AvailabilityViewSet
 
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
 router = DefaultRouter()
 router.register(r"availabilities", AvailabilityViewSet, basename="availability")
 urlpatterns = router.urls
+
+rest_urlpatterns = [
+    path(
+        "availability/<int:pk>/get_availability",
+        views.get_availability,
+        name="get_availability",
+    ),
+    path(
+        "availability/<int:pk>/get_full_availability",
+        views.get_full_availability,
+        name="get_full_availability",
+    ),
+    path(
+        "availability/<int:pk>/set_availability",
+        views.set_availability,
+        name="set_availability",
+    ),
+]
+
+urlpatterns.extend(format_suffix_patterns(rest_urlpatterns))
--- csm_web/logistinator/views.py
+++ csm_web/logistinator/views.py
@@ -1,9 +1,58 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Availability
-from .serializers import AvailabilitySerializer
+from .models import Availability, ImposedEvent
+from .serializers import AvailabilitySerializer, ImposedEventSerializer
+
+from django.db import transaction
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth import logout as auth_logout
+
+from rest_framework import generics, permissions, viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+
+import datetime
 
 # Stubbed Availabiity Model
 class AvailabilityViewSet(viewsets.ModelViewSet):
     queryset = Availability.objects.all()
     serializer_class = AvailabilitySerializer
+
+
+@api_view(http_method_names=["GET"])
+def get_full_availability(request, pk):
+    availability = get_object_or_404(Availability, pk=pk)
+    serialized_availability = AvailabilitySerializer(availability).data
+    return Response(serialized_availability)
+
+
+@api_view(http_method_names=["GET"])
+def get_availability(request, pk):
+    availability = get_object_or_404(Availability, pk=pk)
+    params = request.POST
+    start_time = datetime.datetime.strptime(params["start_time"], "%H:%M").time()
+    end_time = datetime.datetime.strptime(params["end_time"], "%H:%M").time()
+    interval = datetime.timedelta(minutes=int(params["interval"]))
+
+    availability_dictionary = availability.get_weekly_availability(
+        start_time, end_time, interval
+    )
+    return Response(availability_dictionary)
+
+
+@api_view(http_method_names=["POST"])
+def set_availability(request, pk):
+    availability = get_object_or_404(Availability, pk=pk)
+    params = request.POST
+
+    start_time = datetime.datetime.strptime(params["start_time"], "%H:%M").time()
+    end_time = datetime.datetime.strptime(params["end_time"], "%H:%M").time()
+    day = int(params["day"])
+
+    availability._set_availability(day, start_time, end_time)
+    availability.save()
+
+    serialized_availability = AvailabilitySerializer(availability).data
+    return Response(serialized_availability)
