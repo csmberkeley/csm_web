bf501a6b8091923f02f463c0c656b24b1d666665
MrunalPuram <mrunal.puram@gmail.com>
2019-04-18 21:32:33 -0700

    Added endpoints for Availability

--- csm_web/logistinator/views.py
+++ csm_web/logistinator/views.py
@@ -1,4 +1,8 @@
+import datetime
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Availability, ImposedEvent
+from .serializers import AvailabilitySerializer, ImposedEventSerializer
 from django.db import transaction
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
@@ -89,3 +93,40 @@ class DeleteMatching(generics.DestroyAPIView):
 class AvailabilityViewSet(viewsets.ModelViewSet):
     queryset = Availability.objects.all()
     serializer_class = AvailabilitySerializer
+
+
+@api_view(http_method_names=["GET"])
+def get_full_availability(request, pk):
+    availability = get_object_or_404(Availability, pk=pk)
+    serialized_availability = AvailabilitySerializer(availability).data
+    return Response(serialized_availability)
+
+
+@api_view(http_method_names=["GET"])
+def get_availability(request, pk):
+    availability = get_object_or_404(Availability, pk=pk)
+    params = request.POST
+    start_time = datetime.datetime.strptime(params["start_time"], "%H:%M").time()
+    end_time = datetime.datetime.strptime(params["end_time"], "%H:%M").time()
+    interval = datetime.timedelta(minutes=int(params["interval"]))
+
+    availability_dictionary = availability.get_weekly_availability(
+        start_time, end_time, interval
+    )
+    return Response(availability_dictionary)
+
+
+@api_view(http_method_names=["POST"])
+def set_availability(request, pk):
+    availability = get_object_or_404(Availability, pk=pk)
+    params = request.POST
+
+    start_time = datetime.datetime.strptime(params["start_time"], "%H:%M").time()
+    end_time = datetime.datetime.strptime(params["end_time"], "%H:%M").time()
+    day = int(params["day"])
+
+    availability._set_availability(day, start_time, end_time)
+    availability.save()
+
+    serialized_availability = AvailabilitySerializer(availability).data
+    return Response(serialized_availability)
