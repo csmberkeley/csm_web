cd3b399d74eb82bda96ce660064b6501541b5355
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-08-29 13:38:38 -0700

    Add checks to spacetime properties that override not only exists but is still valid, add test cases covering this

--- csm_web/scheduler/test_models.py
+++ csm_web/scheduler/test_models.py
@@ -29,6 +29,17 @@ class SpacetimeTest(TestCase):
         self.assertEqual(self.spacetime.start_time, override.spacetime.start_time)
         self.assertEqual(self.spacetime.duration, override.spacetime.duration)
 
+    def test_spacetime_with_expired_override(self):
+        override = OverrideFactory.create(
+            overriden_spacetime=self.spacetime,
+            date=(datetime.now() - timedelta(days=5)),
+        )
+        self.assertTrue(override.is_expired())
+        self.assertEqual(self.spacetime.location, self.spacetime._location)
+        self.assertEqual(self.spacetime.day_of_week, self.spacetime._day_of_week)
+        self.assertEqual(self.spacetime.start_time, self.spacetime._start_time)
+        self.assertEqual(self.spacetime.duration, self.spacetime._duration)
+
 
 class SectionTest(TestCase):
     def setUp(self):
