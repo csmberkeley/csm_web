6d663a4866245750b91f55aaf0630d75f3a0a694
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-08-29 19:51:03 -0700

    Remove all logstinator code

--- csm_web/logistinator/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class LogistinatorConfig(AppConfig):
-    name = "logistinator"
-
-    def ready(self):
-        # registers signals
-        import logistinator.signals
--- csm_web/logistinator_frontend/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LogistinatorFrontendConfig(AppConfig):
-    name = "logistinator_frontend"
