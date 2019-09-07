15152dbd5591db170d833a062c2c918edac558bf
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-09-07 00:08:04 -0700

    Add additional logging and fix settings, remove django-eventlog, fix enrollment bug I spotted

--- csm_web/scheduler/apps.py
+++ csm_web/scheduler/apps.py
@@ -3,8 +3,3 @@ from django.apps import AppConfig
 
 class SchedulerConfig(AppConfig):
     name = "scheduler"
-
-    def ready(self):
-        # registers signals
-        # pylint:disable=unused-variable
-        import scheduler.signals
