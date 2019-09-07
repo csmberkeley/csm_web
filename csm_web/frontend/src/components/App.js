e5260fffa269d1dc81f6c376551b3879b38ceb35
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-09-07 16:54:40 -0700

    Barebones recreation of App component

--- csm_web/frontend/src/components/App.js
+++ csm_web/frontend/src/components/App.js
@@ -1,174 +1,73 @@
 import React from "react";
 import { MemoryRouter as Router, Route, Redirect } from "react-router-dom";
 import ReactDOM from "react-dom";
-import Section from "./Section";
-import Course from "./Course";
-import CourseNav from "./CourseNav";
-import Navbar from "./Navbar";
+//import Section from "./Section";
+//import Course from "./Course";
+//import CourseNav from "./CourseNav";
+//import Navbar from "./Navbar";
 import { fetchJSON } from "../utils/api";
 
-class App extends React.Component {
-  state = {
-    sections: {},
-    profiles: {},
-    courses: null,
-    profiles_ct: null,
-    ready: false
-  };
+function Courses() {
+  return <div>Courses</div>;
+}
 
-  componentDidMount() {
-    this.updateProfiles();
-    this.updateCourses();
+function Section() {
+  return <div>Section</div>;
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { studentProfiles: null, mentorProfiles: null, currentProfileId: null, ready: false };
+    this.fetchProfiles = this.fetchProfiles.bind(this);
   }
 
-  updateProfiles() {
-    const profilesEndpoint = "profiles/";
-    return fetchJSON(profilesEndpoint).then(profiles => {
-      this.setState(
-        {
-          profiles_ct: profiles.length
-        },
-        () => {
-          for (let profile of profiles) {
-            let id = profile.id;
-            fetchJSON(`${profilesEndpoint}${id}/?verbose=true`)
-              .then(profileData =>
-                this.setState(state => {
-                  let sections = { ...state.sections };
-                  if (profileData.section) {
-                    sections[[profileData.section.id]] = profileData.section;
-                  }
-                  return {
-                    profiles: {
-                      [id]: profileData,
-                      ...state.profiles
-                    },
-                    sections: sections
-                  };
-                })
-              )
-              .then(() => this.updateReadyState());
-          }
-        }
-      );
+  fetchProfiles() {
+    fetchJSON("profiles/").then(({ studentProfiles, mentorProfiles }) => {
+      const currentProfile = studentProfiles[0] || mentorProfiles[0];
+      this.setState({
+        studentProfiles,
+        mentorProfiles,
+        currentProfileId: currentProfile && currentProfile.id,
+        ready: true
+      });
     });
   }
 
-  updateCourses() {
-    return fetchJSON("courses/")
-      .then(courses => {
-        this.setState({
-          courses: courses.reduce((coursesMap, course) => {
-            coursesMap[course.name] = course;
-            return coursesMap;
-          }, {})
-        });
-      })
-      .then(() => this.updateReadyState());
-  }
-
-  updateReadyState() {
-    if (
-      this.state.courses != null &&
-      this.state.profiles_ct != null &&
-      Object.keys(this.state.profiles).length === this.state.profiles_ct
-    ) {
-      this.setState({
-        ready: true
-      });
-    }
+  componentDidMount() {
+    this.fetchProfiles();
   }
 
   render() {
+    if (!this.state.ready) {
+      return <div>Loading...</div>;
+    }
+    const { currentProfileId, mentorProfiles } = this.state;
     return (
-      <div>
-        <Router>
-          <div>
-            <Navbar
-              sections={this.state.sections}
-              courses={this.state.courses}
-            />
-            <Route
-              path="/"
-              exact
-              render={() => {
-                if (!this.state.ready) {
-                  return <LoadingSplash />;
-                } else if (this.state.profiles_ct === 0) {
-                  return <Redirect to="/courses/" push />;
-                } else {
-                  const firstSection = `/sections/${
-                    Object.keys(this.state.sections)[0]
-                  }`;
-                  return <Redirect to={firstSection} push />;
-                }
-              }}
-            />
-            <Route
-              path="/sections/:id"
-              render={({ match }) => {
-                if (!this.state.sections.hasOwnProperty(match.params.id)) {
-                  // This might be a newly added section
-                  this.updateProfiles();
-                  this.updateCourses();
-                  return <LoadingSplash />;
-                } else {
-                  // We lookup the matching profile manually for this state because a
-                  // page linking to this might not have access to profile data. (i.e.
-                  // the Course listing page. This is fine since there are few profiles
-                  // per user.
-
-                  let matchingProfile = null;
-                  for (let [profileID, profile] of Object.entries(
-                    this.state.profiles
-                  )) {
-                    if (
-                      profile.section &&
-                      profile.section.id === Number(match.params.id)
-                    ) {
-                      matchingProfile = profileID;
-                      break;
-                    }
-                  }
-
-                  // TODO might need to catch null fallthrough here
-                  return (
-                    <Section
-                      profile={matchingProfile}
-                      key={match.params.id}
-                      {...this.state.sections[match.params.id]}
-                    />
-                  );
-                }
-              }}
-            />
-            <Route
-              path="/courses/"
-              exact
-              render={() => <CourseNav courses={this.state.courses} />}
-            />
-            <Route
-              path="/courses/:slug"
-              render={({ match }) => (
-                <Course course={this.state.courses[match.params.slug]} />
-              )}
-            />
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <Redirect to={currentProfileId == undefined ? "/courses/" : `/sections/${currentProfileId}/`} />
+            )}
+          />
+          <Route
+            path="/sections/:id"
+            render={routeProps => (
+              <Section
+                isMentor={mentorProfiles.map(profile => profile.id).includes(currentProfileId)}
+                {...routeProps}
+              />
+            )}
+          />
+          <Route path="/courses/" component={Courses} />
+        </React.Fragment>
+      </Router>
     );
   }
 }
 
-function LoadingSplash() {
-  return (
-    <div className="loading-splash">
-      <h3>Loading...</h3>
-    </div>
-  );
-}
-
 const wrapper = document.getElementById("app");
-
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
-export default App;
