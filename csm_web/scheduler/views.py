802b544ac3065b0138d09f2c958fc8d3c560e5ee
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-09-06 20:02:37 -0700

    Finish permissions

--- csm_web/scheduler/views.py
+++ csm_web/scheduler/views.py
@@ -4,16 +4,33 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import PermissionDenied
 from .models import Course, Section, Student, Spacetime
 from rest_framework import mixins
 from .serializers import CourseSerializer, SectionSerializer, StudentSerializer, AttendanceSerializer, MentorSerializer, OverrideSerializer
+from django.core.exceptions import ObjectDoesNotExist
+
+
+def get_object_or_error(specific_queryset, **kwargs):
+    """
+    Look up an object by kwargs in specific_queryset. If it exists there,
+    return it. If the object exists but is not within the scope of the specific_queryset,
+    raise a permission error (403), otherwise if the object does not exist at all raise a 404.
+    """
+    try:
+        return specific_queryset.get(**kwargs)
+    except ObjectDoesNotExist:
+        if get_object_or_404(specific_queryset.model.objects, **kwargs):
+            raise PermissionDenied()
+
 
 METHOD_MIXINS = {'list': mixins.ListModelMixin, 'create': mixins.CreateModelMixin,
-                 'retrieve': mixins.RetrieveModelMixin, 'update': mixins.UpdateModelMixin, 'partial_update': mixins.UpdateModelMixin, 'destroy': mixins.DestroyModelMixin}
+                 'retrieve': mixins.RetrieveModelMixin, 'update': mixins.UpdateModelMixin,
+                 'partial_update': mixins.UpdateModelMixin, 'destroy': mixins.DestroyModelMixin}
 
 
 def viewset_with(*permitted_methods):
-    assert all(method in METHOD_MIXINS for method in permitted_methods)
+    assert all(method in METHOD_MIXINS for method in permitted_methods), "Unrecognized method for ViewSet"
     return list(set(mixin_class for method, mixin_class in METHOD_MIXINS.items() if method in permitted_methods)) + [viewsets.GenericViewSet]
 
 
@@ -23,21 +40,24 @@ class CourseViewSet(*viewset_with('list')):
 
     @action(detail=True)
     def sections(self, request, pk=None):
-        course = get_object_or_404(self.queryset, pk=pk)
+        course = get_object_or_error(self.queryset, pk=pk)
         return Response(SectionSerializer(course.section_set.all(), many=True).data)
 
 
 class SectionViewSet(*viewset_with('retrieve')):
     serializer_class = SectionSerializer
 
+    def get_object(self):
+        return get_object_or_error(self.get_queryset(), **self.kwargs)
+
     def get_queryset(self):
         mentor_sections = Section.objects.filter(mentor__user=self.request.user)
         student_sections = Section.objects.filter(students__user=self.request.user)
-        return mentor_sections.union(student_sections)
+        return mentor_sections | student_sections
 
     @action(detail=True, methods=['get', 'put'])
     def students(self, request, pk=None):
-        section = get_object_or_404(self.queryset, pk=pk)
+        section = get_object_or_error(self.get_queryset(), pk=pk)
         if request.method == 'GET':
             return Response(StudentSerializer(section.students.filter(active=True), many=True).data)
         # PUT
@@ -58,25 +78,31 @@ class StudentViewSet(viewsets.GenericViewSet):
     def get_queryset(self):
         own_profiles = Student.objects.filter(user=self.request.user)
         pupil_profiles = Student.objects.filter(section__mentor__user=self.request.user)
-        return own_profiles.union(pupil_profiles)
+        return own_profiles | pupil_profiles
 
     @action(detail=True, methods=['patch'])
     def drop(self, request, pk=None):
-        student = get_object_or_404(self.queryset, pk=pk)
+        student = get_object_or_error(self.get_queryset(), pk=pk)
         student.active = False
         student.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-    @action(detail=True, methods=['get', 'post'])
+    @action(detail=True, methods=['get', 'put'])
     def attendances(self, request, pk=None):
-        student = get_object_or_404(self.queryset, pk=pk)
+        student = get_object_or_error(self.get_queryset(), pk=pk)
         if request.method == 'GET':
             return Response(AttendanceSerializer(student.attendance_set.all(), many=True).data)
-        # POST
-        serializer = AttendanceSerializer(data={'student': student.id, **request.data})
+        # PUT
+        if student.user == self.request.user:
+            raise PermissionDenied('You cannot record your own attendance (nice try)')
+        try:  # update
+            attendance = student.attendance_set.get(date=request.data['date'])
+            serializer = AttendanceSerializer(attendance, data={'student': student.id, **request.data})
+        except ObjectDoesNotExist:  # create
+            serializer = AttendanceSerializer(data={'student': student.id, **request.data})
         if serializer.is_valid():
             serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
 
@@ -91,17 +117,17 @@ class ProfileViewSet(*viewset_with('list')):
 
 
 class SpacetimeViewSet(viewsets.GenericViewSet):
-    serializer_class = None
+    serializer_class = Spacetime
 
     def get_queryset(self):
         return Spacetime.objects.filter(section__mentor__user=self.request.user)
 
     @action(detail=True, methods=['put'])
     def override(self, request, pk=None):
-        spacetime = get_object_or_404(self.queryset, pk=pk)
-        if hasattr(spacetime, "override"):
+        spacetime = get_object_or_error(self.get_queryset(), pk=pk)
+        if hasattr(spacetime, "override"):  # update
             serializer = OverrideSerializer(spacetime.override, data=request.data)
-        else:
+        else:  # create
             serializer = OverrideSerializer(data={'overriden_spacetime': spacetime, **request.data})
         if serializer.is_valid():
             serializer.save()
