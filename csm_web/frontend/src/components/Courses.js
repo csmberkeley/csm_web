7bae5da365745f8e34177817c1673ffa21a26482
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-09-07 18:26:05 -0700

    Cache sections for each course, add nav links to switch between them

--- csm_web/frontend/src/components/Courses.js
+++ csm_web/frontend/src/components/Courses.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-//import { Redirect } from "react-router-dom";
-//import { groupBy } from "lodash";
-//import moment from "moment";
+import { Route, NavLink } from "react-router-dom";
 import { fetchJSON } from "../utils/api";
 import PropTypes from "prop-types";
-
-//import { alert_modal } from "../utils/common";
 
 export default class Courses extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentCourseId: null, courses: null, ready: false };
+    this.state = { currentCourseId: null, courses: null, ready: false, sectionCache: {} };
     this.fetchCourses = this.fetchCourses.bind(this);
+    this.updateSectionCache = this.updateSectionCache.bind(this);
   }
 
   static propTypes = { match: PropTypes.object };
@@ -21,6 +17,10 @@ export default class Courses extends React.Component {
     return fetchJSON(this.props.match.url).then(courses =>
       this.setState({ courses, currentCourseId: courses[0] && courses[0].id, ready: true })
     );
+  }
+
+  updateSectionCache(courseId, sections) {
+    this.setState(state => ({ sectionCache: { ...state.sectionCache, [courseId]: sections } }));
   }
 
   componentDidMount() {
@@ -33,11 +33,21 @@ export default class Courses extends React.Component {
     }
     return (
       <div>
-        <Route path={`${this.props.match.path}/:id`} component={Course} />
+        {this.state.courses.map(course => (
+          <NavLink key={course.id} to={`${this.props.match.path}/${course.id}`}>
+            {course.name}
+          </NavLink>
+        ))}
         <Route
-          exact
-          path={this.props.match.path}
-          render={() => <Redirect to={`${this.props.match.path}/${this.state.currentCourseId}`} />}
+          path={`${this.props.match.path}/:id`}
+          render={routeProps => (
+            <Course
+              updateSectionCache={this.updateSectionCache}
+              cachedSections={this.state.sectionCache[routeProps.match.params.id]}
+              key={routeProps.match.params.id}
+              {...routeProps}
+            />
+          )}
         />
         {JSON.stringify(this.state)}
       </div>
@@ -52,20 +62,27 @@ class Course extends React.Component {
     this.fetchSections = this.fetchSections.bind(this);
   }
 
-  static propTypes = { match: PropTypes.object };
+  static propTypes = { match: PropTypes.object, cachedSections: PropTypes.array, updateSectionCache: PropTypes.func };
 
   fetchSections() {
-    fetchJSON(`${this.props.match.url}/sections`).then(sections => this.setState({ sections, ready: true }));
+    return fetchJSON(`${this.props.match.url}/sections`).then(sections => {
+      this.setState({ sections, ready: true });
+      return sections;
+    });
   }
 
   componentDidMount() {
-    this.fetchSections();
+    if (!this.props.cachedSections) {
+      this.fetchSections().then(sections => this.props.updateSectionCache(this.props.match.params.id, sections));
+    } else {
+      this.setState({ ready: true });
+    }
   }
 
   render() {
     if (!this.state.ready) {
       return <div>Loading sections for course...</div>;
     }
-    return <div>{JSON.stringify(this.state)}</div>;
+    return <div>{JSON.stringify(this.props.cachedSections || this.state.sections)}</div>;
   }
 }
