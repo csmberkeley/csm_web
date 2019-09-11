3207e36bd881029298b97b7a2510a5411e4f81b3
noloerino <jhshi07@gmail.com>
2019-09-10 19:21:19 -0700

    Add logo

--- csm_web/frontend/src/components/Navbar.js
+++ csm_web/frontend/src/components/Navbar.js
@@ -1,81 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import moment from "moment";
+import PropTypes from "prop-types";
 
-function SectionDropdownEntry(props) {
-  const displayTime = moment(
-    props.section.defaultSpacetime.startTime,
-    "HH:mm:ss"
-  ).format("hh:mm A");
-  return (
-    <div className="section-dropdown-entry">
-      <h3 className="section-dropdown-entry-course-label">
-        {props.section.courseName}
-      </h3>
-      <p className="section-dropdown-entry-time">
-        {props.section.defaultSpacetime.dayOfWeek} {displayTime}
-      </p>
-    </div>
-  );
-}
+const profileShape = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  sectionId: PropTypes.number.isRequired,
+  course: PropTypes.string.isRequired
+});
 
-function Navbar(props) {
-  const sectionListEntries = Object.entries(props.sections).map(entry => {
-    const [id, section] = entry;
+export default class Navbar extends React.Component {
+  static propTypes = {
+    studentProfiles: PropTypes.arrayOf(profileShape).isRequired,
+    mentorProfiles: PropTypes.arrayOf(profileShape).isRequired
+  };
+
+  render() {
     return (
-      <li key={id}>
-        <Link to={`/sections/${id}`}>
-          <SectionDropdownEntry section={section} />
-        </Link>
-      </li>
-    );
-  });
-
-  return (
-    <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-      <nav
-        id="navbar"
-        uk-navbar="true"
-        className="uk-navbar-container"
-        data-uk-navbar="delay-hide: 200"
-      >
-        <div className="logo-container uk-navbar-item">
-          <img
-            className="logo-img uk-logo"
-            src="static/frontend/img/logo-min/logo_notext_white.png"
-          />
-        </div>
-        <div className="uk-navbar-right">
-          <ul className="uk-navbar-nav">
-            <li className="uk-active">
-              <a id="section-btn" href="#">
-                <span style={{ marginRight: `${0.2}em` }} uk-icon="menu" />{" "}
-                Sections
-              </a>
-              <div
-                id="section-dropdown"
-                className="uk-navbar-dropdown"
-                uk-dropdown="offset: 0; pos: bottom-right;"
-              >
-                <ul className="uk-nav uk-navbar-dropdown-nav">
-                  {sectionListEntries}
-                  <Link to={"/courses/"}>
-                    <li id="section-enroll-btn">
-                      <span
-                        uk-icon="icon: plus; ratio: 0.8"
-                        uk-icon-button="true"
-                      />{" "}
-                      Enroll in a section
-                    </li>
-                  </Link>
-                </ul>
-              </div>
-            </li>
-          </ul>
+      <nav id="navbar">
+        <div className="logo-container">
+          <Link to="/">
+            <img src="static/frontend/img/logo-min/logo_notext.png" style={{ height: "3em" }} />
+          </Link>
         </div>
       </nav>
-    </div>
-  );
+    );
+  }
 }
-
-export default Navbar;
