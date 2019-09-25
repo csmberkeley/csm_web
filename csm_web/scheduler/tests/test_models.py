3e0d2d9be5967892dad561647fbae2ac38ae4887
Svetlitski <kevin_svetlitski@berkeley.edu>
2019-09-24 20:48:33 -0700

    Add testcase for location normalization

--- csm_web/scheduler/tests/test_models.py
+++ csm_web/scheduler/tests/test_models.py
@@ -15,8 +15,8 @@ from scheduler.factories import (
 class SpacetimeTest(TestCase):
     def setUp(self):
         course = CourseFactory.create()
-        section = SectionFactory.create(course=course)
-        self.spacetime = SpacetimeFactory.create(section=section)
+        self.section = SectionFactory.create(course=course)
+        self.spacetime = SpacetimeFactory.create(section=self.section)
 
     def test_spacetime_without_override(self):
         self.assertEqual(self.spacetime.location, self.spacetime._location)
@@ -43,6 +43,10 @@ class SpacetimeTest(TestCase):
         self.assertEqual(self.spacetime.day_of_week, self.spacetime._day_of_week)
         self.assertEqual(self.spacetime.start_time, self.spacetime._start_time)
         self.assertEqual(self.spacetime.duration, self.spacetime._duration)
+
+    def test_location_normalization(self):
+        spacetime = SpacetimeFactory.create(_location=' sODa     372 ', section=self.section)
+        self.assertEqual(spacetime._location, 'Soda 372')
 
 
 class CourseTest(TestCase):
