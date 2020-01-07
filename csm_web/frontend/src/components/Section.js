bf59c4d69c1d0ad02f2eee534977411530d7d2fd
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-01-06 18:47:16 -0600

    Add route for section details

--- csm_web/frontend/src/components/Section.js
+++ csm_web/frontend/src/components/Section.js
@@ -1,74 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { fetchJSON } from "../utils/api";
-import SectionDetail from "./SectionDetail";
-import { AsStudentAttendance, AsMentorAttendance } from "./Attendance";
 
 export default class Section extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { ready: false };
-    this.fetchInfo = this.fetchInfo.bind(this);
-  }
-
   static propTypes = {
-    match: PropTypes.object,
-    isMentor: PropTypes.bool,
-    currentProfileId: PropTypes.number
+    match: PropTypes.shape({ params: PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired }).isRequired
   };
 
-  fetchInfo() {
-    Promise.all([
-      fetchJSON(this.props.match.url).then(sectionInfo => this.setState({ sectionInfo: sectionInfo })),
-      this.props.isMentor
-        ? fetchJSON(`${this.props.match.url}/students`).then(students => this.setState({ students }))
-        : fetchJSON(`/students/${this.props.currentProfileId}/attendances`).then(attendances =>
-            this.setState({ attendances })
-          )
-    ]).then(() => this.setState({ ready: true }));
-  }
+  state = { section: null, loaded: false };
 
   componentDidMount() {
-    this.fetchInfo();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.match.params.id != this.props.match.params.id) {
-      this.setState({ ready: false });
-      this.fetchInfo();
-    }
+    fetchJSON(`/sections/${this.props.match.params.id}`).then(section => this.setState({ section, loaded: true }));
   }
 
   render() {
-    return (
-      <div>
-        {/* Displays section info */}
-        <h2>Section</h2>
-        {this.state.ready && (
-          <SectionDetail
-            key={this.props.match.params.id}
-            sectionInfo={this.state.sectionInfo}
-            isStudent={!this.props.isMentor}
-            profileId={this.props.currentProfileId}
-            students={this.state.students}
-          />
-        )}
-        {/* Displays attendances*/}
-        {this.state.ready ? (
-          <React.Fragment>
-            <h4>Attendances</h4>
-            <div>
-              {this.props.isMentor ? (
-                <AsMentorAttendance students={this.state.students} />
-              ) : (
-                <AsStudentAttendance attendances={this.state.attendances} />
-              )}
-            </div>
-          </React.Fragment>
-        ) : (
-          <div>Loading attendances...</div>
-        )}
-      </div>
-    );
+    const { section, loaded } = this.state;
+    return !loaded ? null : section.isStudent ? <StudentSection {...section} /> : <MentorSection {...section} />;
   }
+}
+
+function StudentSection() {
+  return <div>I am a student</div>;
+}
+
+function MentorSection() {
+  return <div>I am a mentor</div>;
 }
