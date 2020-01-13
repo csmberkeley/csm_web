7e0053e94c33dceaa563a90f0e613de04a86b337
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-01-12 23:11:28 -0600

    Make api tests compatible with Spacetime model changes

--- csm_web/scheduler/tests/test_api.py
+++ csm_web/scheduler/tests/test_api.py
@@ -20,7 +20,7 @@ class ProfileListTest(APITestCase):
     def test_simple_mentor(self):
         mentor = MentorFactory.create(user=self.user)
         section = SectionFactory.create(mentor=mentor, course=CourseFactory.create(name='CS61C', title='Machine Structures'),
-                                        spacetime=SpacetimeFactory.create(_start_time=time(hour=13), _day_of_week='Mon', _duration=timedelta(hours=1)))
+                                        spacetime=SpacetimeFactory.create(start_time=time(hour=13), day_of_week='Mon', duration=timedelta(hours=1)))
         response = self.client.get(self.endpoint)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0], {'id': mentor.pk, 'section_id': section.pk, 'section_spacetime': 'Monday 1:00-2:00 PM',
@@ -28,7 +28,7 @@ class ProfileListTest(APITestCase):
 
     def test_simple_student(self):
         section = SectionFactory.create(course=CourseFactory.create(name='CS61C', title='Machine Structures'),
-                                        spacetime=SpacetimeFactory.create(_start_time=time(hour=13), _day_of_week='Mon', _duration=timedelta(hours=1)))
+                                        spacetime=SpacetimeFactory.create(start_time=time(hour=13), day_of_week='Mon', duration=timedelta(hours=1)))
         student = StudentFactory.create(user=self.user, section=section)
         response = self.client.get(self.endpoint)
         self.assertEqual(len(response.data), 1)
@@ -37,11 +37,11 @@ class ProfileListTest(APITestCase):
 
     def test_mentor_and_student(self):
         student_section = SectionFactory.create(course=CourseFactory.create(name='CS61C', title='Machine Structures'),
-                                                spacetime=SpacetimeFactory.create(_start_time=time(hour=13), _day_of_week='Mon', _duration=timedelta(hours=1)))
+                                                spacetime=SpacetimeFactory.create(start_time=time(hour=13), day_of_week='Mon', duration=timedelta(hours=1)))
         student = StudentFactory.create(user=self.user, section=student_section)
         mentor = MentorFactory.create(user=self.user)
         mentor_section = SectionFactory.create(mentor=mentor, course=CourseFactory.create(name='CS70', title='Discrete Mathematics and Probability Theory'),
-                                               spacetime=SpacetimeFactory.create(_start_time=time(hour=11), _day_of_week='Tue', _duration=timedelta(hours=1.5)))
+                                               spacetime=SpacetimeFactory.create(start_time=time(hour=11), day_of_week='Tue', duration=timedelta(hours=1.5)))
         response = self.client.get(self.endpoint)
         self.assertEqual(len(response.data), 2)
         self.assertTrue({'id': student.pk, 'section_id': student_section.pk, 'section_spacetime': 'Monday 1:00-2:00 PM',
@@ -51,7 +51,7 @@ class ProfileListTest(APITestCase):
 
     def test_inactive_student(self):
         section = SectionFactory.create(course=CourseFactory.create(name='CS61C', title='Machine Structures'),
-                                        spacetime=SpacetimeFactory.create(_start_time=time(hour=13), _day_of_week='Mon', _duration=timedelta(hours=1)))
+                                        spacetime=SpacetimeFactory.create(start_time=time(hour=13), day_of_week='Mon', duration=timedelta(hours=1)))
         StudentFactory.create(user=self.user, section=section, active=False)
         response = self.client.get(self.endpoint)
         self.assertEqual(len(response.data), 0)
