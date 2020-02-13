cc40c8867261ca8c3a4ae232b1801be84a47e426
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-02-12 17:17:13 -0800

    Add GZip middleware because some API responses are huge in reality (/api/sections for CS61A is 40KB uncompressed in production)

--- csm_web/csm_web/settings.py
+++ csm_web/csm_web/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.gzip.GZipMiddleware"
 ]
 
 ROOT_URLCONF = "csm_web.urls"
