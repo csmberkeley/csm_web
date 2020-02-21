0a864fc91178939198553b9f700ccbae5319403c
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-02-21 01:14:35 -0800

    Get rid of annoying nodemon notifications

--- .nodemon.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [[ "$(uname)" == 'Darwin' ]] # if on macOS
-then
-	npm run dev && osascript -e 'display notification "npm run dev has finished" with title "Build complete" sound name "default"'
-else
-	npm run dev
-fi
