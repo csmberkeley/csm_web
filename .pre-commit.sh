9ee2159cddd8b86e4da04096a9095fc3dd58188c
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-02-20 19:03:21 -0800

    Reformat all JSON config files with Prettier for consistency, update javascript dev dependencies

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -5,12 +5,18 @@ reformatted_files=$(mktemp)
 # Look only at staged files that have been modified, renamed, or newly added
 git diff --cached --name-only --diff-filter=MRA > "$staged_files"
 
-javascript_files=$(grep -E '.*/src/.*\.jsx?$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
+javascript_files=$(grep -E -e '.*/src/.*\.jsx?$' -e 'webpack\.config\.js$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
+json_files=$(grep -E '\.json$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
 
 if [ -n "$javascript_files" ]
 then
 	npx eslint --quiet $javascript_files || exit 1
 	npx prettier --write --list-different $javascript_files >> "$reformatted_files"
+fi
+
+if [ -n "$json_files" ]
+then
+	npx prettier --write --list-different $json_files >> "$reformatted_files"
 fi
 
 python_files=$(grep '\.py$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
