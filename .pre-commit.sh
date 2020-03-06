219d6a89b67ec64fa91578d7e12c79ba97bce652
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-03-06 13:02:49 -0800

    Reformat CSS with prettier, add to pre-commit hook

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -5,8 +5,8 @@ reformatted_files=$(mktemp)
 # Look only at staged files that have been modified, renamed, or newly added
 git diff --cached --name-only --diff-filter=MRA > "$staged_files"
 
-javascript_files=$(grep -E -e '.*/src/.*\.jsx?$' -e 'webpack\.config\.js$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
-json_files=$(grep -E '\.json$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
+javascript_files=$(grep -E -e '.*/src/.*\.jsx?$' -e  "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
+format_only_files=$(grep -E -e '\.json$' -e 'webpack\.config\.js$' -e '\.css$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
 
 if [ -n "$javascript_files" ]
 then
@@ -14,9 +14,9 @@ then
 	npx prettier --write --list-different $javascript_files >> "$reformatted_files"
 fi
 
-if [ -n "$json_files" ]
+if [ -n "$format_only_files" ]
 then
-	npx prettier --write --list-different $json_files >> "$reformatted_files"
+	npx prettier --write --list-different $format_only_files >> "$reformatted_files"
 fi
 
 python_files=$(grep '\.py$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
