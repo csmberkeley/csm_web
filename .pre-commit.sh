10abc364744c1d5472155eec4d21161456788b2b
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-03-12 19:12:12 -0500

    Fix bug preventing pre-commit hook from reformatting JS files

--- .pre-commit.sh
+++ .pre-commit.sh
@@ -5,7 +5,7 @@ reformatted_files=$(mktemp)
 # Look only at staged files that have been modified, renamed, or newly added
 git diff --cached --name-only --diff-filter=MRA > "$staged_files"
 
-javascript_files=$(grep -E -e '.*/src/.*\.jsx?$' -e  "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
+javascript_files=$(grep -E -e '.*/src/.*\.jsx?$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
 format_only_files=$(grep -E -e '\.json$' -e 'webpack\.config\.js$' -e '\.css$' "$staged_files" | tr '\n' ' ' | sed 's/ *$//')
 
 if [ -n "$javascript_files" ]
