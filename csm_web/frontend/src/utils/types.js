4ffb42af44f75f58a1f5e8f9d85f15e01b966169
noloerino <jhshi07@gmail.com>
2020-08-20 23:59:06 -0700

    Update SPACETIME_SHAPE

--- csm_web/frontend/src/utils/types.js
+++ csm_web/frontend/src/utils/types.js
@@ -1,5 +1,9 @@
 import PropTypes from "prop-types";
 export const SPACETIME_SHAPE = PropTypes.shape({
+  dayOfWeek: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
   location: PropTypes.string.isRequired,
+  startTime: PropTypes.string.isRequired,
   time: PropTypes.string.isRequired
 });
