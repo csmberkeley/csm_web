a1a7cbcd2a2b1ea3a4d30ba557b7198427ab9887
Svetlitski <kevin_svetlitski@berkeley.edu>
2020-09-07 18:29:39 -0500

    Add an Error Boundary so that users get a useful error message instead of a blank white screen

--- csm_web/frontend/src/components/App.js
+++ csm_web/frontend/src/components/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 import CourseMenu from "./CourseMenu";
 import Home from "./Home";
 import Section from "./Section";
@@ -8,7 +9,20 @@ import LogoNoText from "../../static/frontend/img/logo_no_text.svg";
 import LogOutIcon from "../../static/frontend/img/log_out.svg";
 
 export default class App extends React.Component {
+  state = { error: null };
+
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
+
+  clearError = () => {
+    this.setState({ error: null });
+  };
+
   render() {
+    if (this.state.error) {
+      return <ErrorPage error={this.state.error} clearError={this.clearError} />;
+    }
     return (
       <Router>
         <React.Fragment>
@@ -39,6 +53,45 @@ function Header() {
     </header>
   );
 }
+
+function ErrorPage({ error: { message, stack }, clearError }) {
+  useEffect(() => {
+    const prevHistoryFunc = window.onpopstate;
+    window.onpopstate = () => {
+      clearError();
+      window.onpopstate = prevHistoryFunc;
+    };
+  }, [clearError]);
+  return (
+    <React.Fragment>
+      <main id="fatal-error-page">
+        <h1>A fatal error has occurred!</h1>
+        <h2 className="page-title">If you&apos;re a mentor:</h2>
+        <p>
+          Please report this on the Slack channel #tech-ask. Include a screenshot of this page along with an explanation
+          of what you were trying to accomplish when you encountered this error
+        </p>
+        <h2 className="page-title">If you&apos;re a student:</h2>
+        <p>
+          Please email <a href="mailto:mentors@berkeley.edu">mentors@berkeley.edu</a> with subject line &quot;[Scheduler
+          Error Report]&quot; with a screenshot of this page along with an explanation of what you were trying to
+          accomplish when you encountered this error
+        </p>
+      </main>
+      <div id="stack-trace">
+        <pre>
+          {message + "\n\n"}
+          {stack}
+        </pre>
+      </div>
+    </React.Fragment>
+  );
+}
+
+ErrorPage.propTypes = {
+  error: PropTypes.shape({ message: PropTypes.string.isRequired, stack: PropTypes.string.isRequired }).isRequired,
+  clearError: PropTypes.func.isRequired
+};
 
 const wrapper = document.getElementById("app");
 wrapper ? ReactDOM.render(<App />, wrapper) : null;
