d8af4ad3f3095f3fff248b144a8be5836c0a5a06
smartspot2 <smartspot2@gmail.com>
2021-07-16 18:49:47 -0700

    Add get user roles function

---
+++ csm_web/frontend/src/utils/user.tsx
@@ -0,0 +1,22 @@
+import { fetchJSON } from "./api";
+import { cloneDeep } from "lodash";
+
+const DEFAULT_ROLES = {
+    'STUDENT': new Set(),
+    'MENTOR': new Set(),
+    'COORDINATOR': new Set(),
+};
+
+function getRoles() {  // TODO: determine what type to return
+    const roles = cloneDeep(DEFAULT_ROLES);
+
+    fetchJSON("/profiles").then(
+        profiles => {
+            profiles.map(course => {
+                roles[course.role].add(course.course);
+            });
+        }
+    );
+
+    return roles;
+}
