ac5a0185a09832e4bc1b545c5b0cdb941b37a4b3
Naveen Gopalan <ngopalan@berkeley.edu>
2021-08-12 19:40:49 -0700

    prettified utils/user.tsx

--- csm_web/frontend/src/utils/user.tsx
+++ csm_web/frontend/src/utils/user.tsx
@@ -2,21 +2,20 @@ import { fetchJSON } from "./api";
 import { cloneDeep } from "lodash";
 
 const DEFAULT_ROLES = {
-    'STUDENT': new Set(),
-    'MENTOR': new Set(),
-    'COORDINATOR': new Set(),
+  STUDENT: new Set(),
+  MENTOR: new Set(),
+  COORDINATOR: new Set()
 };
 
-function getRoles() {  // TODO: determine what type to return
-    const roles = cloneDeep(DEFAULT_ROLES);
+export function getRoles() {
+  // TODO: determine what type to return
+  const roles = cloneDeep(DEFAULT_ROLES);
 
-    fetchJSON("/profiles").then(
-        profiles => {
-            profiles.map(course => {
-                roles[course.role].add(course.course);
-            });
-        }
-    );
+  fetchJSON("/profiles").then(profiles => {
+    profiles.map(course => {
+      roles[course.role].add(course.course);
+    });
+  });
 
-    return roles;
+  return roles;
 }
