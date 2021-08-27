d4db4dc34a2c823c5b186bd70b3135b836f34c6b
smartspot2 <smartspot2@gmail.com>
2021-08-26 20:27:06 -0700

    Add confirmation modal for resource deletion

--- csm_web/frontend/src/components/resource_aggregation/ResourceRowRender.tsx
+++ csm_web/frontend/src/components/resource_aggregation/ResourceRowRender.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import Modal from "../Modal";
 
 const ResourceTopics = ({ topics }) => {
   if (topics === undefined) return <div></div>;
@@ -12,47 +13,66 @@ const ResourceTopics = ({ topics }) => {
 };
 
 const ResourceRowRender = ({ resource, canEdit, onSetEdit, onDelete }) => {
+  /**
+   * Deletion stages:
+   * 0: initial; did not click delete button
+   * 1: confirmation; clicked delete button, asking for confirmation in modal
+   */
+  const [deletionStage, setDeletionStage] = useState(0);
   return (
-    <div className="resourceContainer">
-      <div className="resourceInfo weekNum">
-        <div>Week {resource.weekNum}</div>
-      </div>
-      <div className="resourceInfo dateCell">
-        <div>{resource.date}</div>
-      </div>
-      <div className="resourceInfo resourceTopics">
-        <ResourceTopics topics={resource.topics} />
-      </div>
-      <div className="resourceWkstFilesContainer">
-        {resource.worksheets &&
-          resource.worksheets.map(worksheet => (
-            <div key={worksheet.id} className="resourceWkst">
-              <div className="resourceWkstFile">
-                <a href={worksheet.worksheetFile} target="_blank">
-                  {worksheet.name}
-                </a>
-              </div>
-              {worksheet.solutionFile && (
-                <div className="resourceSoln">
-                  <a href={worksheet.solutionFile} target="_blank">
-                    Solutions
+    <>
+      {deletionStage === 1 && (
+        <Modal className="resourceDeleteConfirmation" closeModal={() => setDeletionStage(0)}>
+          <div className="resourceDeleteText">
+          <h2>Are you sure you want to delete this resource?</h2>
+          <p>This action is irreversible!</p>
+          <button className="danger-btn" onClick={() => onDelete(resource.id)}>
+            Confirm
+          </button>
+          </div>
+        </Modal>
+      )}
+      <div className="resourceContainer">
+        <div className="resourceInfo weekNum">
+          <div>Week {resource.weekNum}</div>
+        </div>
+        <div className="resourceInfo dateCell">
+          <div>{resource.date}</div>
+        </div>
+        <div className="resourceInfo resourceTopics">
+          <ResourceTopics topics={resource.topics} />
+        </div>
+        <div className="resourceWkstFilesContainer">
+          {resource.worksheets &&
+            resource.worksheets.map(worksheet => (
+              <div key={worksheet.id} className="resourceWkst">
+                <div className="resourceWkstFile">
+                  <a href={worksheet.worksheetFile} target="_blank">
+                    {worksheet.name}
                   </a>
                 </div>
-              )}
-            </div>
-          ))}
-      </div>
-      {canEdit && (
-        <div className="resourceButtonsContainer">
-          <button onClick={onSetEdit} className="resourceButton">
-            <FontAwesomeIcon icon={faPencilAlt} id="editIcon" />
-          </button>
-          <button onClick={() => onDelete(resource.id)} className="resourceButton">
-            <FontAwesomeIcon icon={faTrashAlt} id="deleteIcon" />
-          </button>
+                {worksheet.solutionFile && (
+                  <div className="resourceSoln">
+                    <a href={worksheet.solutionFile} target="_blank">
+                      Solutions
+                    </a>
+                  </div>
+                )}
+              </div>
+            ))}
         </div>
-      )}
-    </div>
+        {canEdit && (
+          <div className="resourceButtonsContainer">
+            <button onClick={onSetEdit} className="resourceButton">
+              <FontAwesomeIcon icon={faPencilAlt} id="editIcon" />
+            </button>
+            <button onClick={() => setDeletionStage(1)} className="resourceButton">
+              <FontAwesomeIcon icon={faTrashAlt} id="deleteIcon" />
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
