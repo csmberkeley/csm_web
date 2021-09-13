0433e62c47e417a02d9afaa13a6df281a14a8796
Rohan D'Souza <rodsouza9@gmail.com>
2021-09-13 15:00:55 -0700

    wrap atomic

--- csm_web/scheduler/management/commands/create_attendances.py
+++ csm_web/scheduler/management/commands/create_attendances.py
@@ -5,6 +5,7 @@ from psqlextra.util import postgres_manager
 from psqlextra.types import ConflictAction
 from datetime import timedelta
 import logging
+from django.db import transaction
 
 
 logger = logging.getLogger(__name__)
@@ -18,31 +19,33 @@ class Command(BaseCommand):
         week_start, _ = week_bounds(timezone.now().date())
         sections = Section.objects.all()
         print(f"Updating attendance for week of {week_start}")
-        with postgres_manager(SectionOccurrence) as so_db, postgres_manager(Attendance) as attend_db:
-            so_manager = so_db.on_conflict(["date", "section"], ConflictAction.NOTHING)
-            attendance_manager = attend_db.on_conflict(["sectionOccurrence", "student"], ConflictAction.NOTHING)
+        with transaction.atomic():
+            with postgres_manager(SectionOccurrence) as so_db, postgres_manager(Attendance) as attend_db:
+                so_manager = so_db.on_conflict(["date", "section"], ConflictAction.NOTHING)
+                attendance_manager = attend_db.on_conflict(["sectionOccurrence", "student"], ConflictAction.NOTHING)
 
-            sos = [{"date": week_start + timedelta(days=day_to_number(spacetime.day_of_week)),
-                    "section": section} for section in sections for spacetime in section.spacetimes.all()]
-            sos = so_manager.bulk_insert(sos, return_model=True)
-            print(f"Inserted {len(sos)} SectionOccurrences.")
+                sos = [{"date": week_start + timedelta(days=day_to_number(spacetime.day_of_week)),
+                        "section": section} for section in sections for spacetime in section.spacetimes.all()]
+                sos = so_manager.bulk_insert(sos, return_model=True)
+                print(f"Inserted {len(sos)} SectionOccurrences.")
 
-            attendances = [{"student": student, "sectionOccurrence": so, "presence": ''}
-                           for so in sos for student in so.section.students.all()]
+                attendances = [{"student": student, "sectionOccurrence": so, "presence": ''}
+                               for so in sos for student in so.section.students.all()]
 
-            # validate attendances
-            valid = True
-            for attendance in attendances:
-                student = attendance.get("student")
-                SO = attendance.get("sectionOccurrence")
-                if student and SO:
-                    if student.section.pk != SO.section.pk:
-                        valid = False
-                        logger.error(f"<Logging> student.section.pk != SO.section.pk, attendance to add: {attendances}")
-                        break
+                # validate attendances
+                valid = True
+                for attendance in attendances:
+                    student = attendance.get("student")
+                    SO = attendance.get("sectionOccurrence")
+                    if student and SO:
+                        if student.section.pk != SO.section.pk:
+                            valid = False
+                            logger.error(
+                                f"<Logging> student.section.pk != SO.section.pk, attendance to add: {attendances}")
+                            break
 
-            if attendances and valid:
-                inserted = attendance_manager.bulk_insert(attendances)
-                print(f"Inserted {len(inserted)} attendances.")
-            else:
-                print("No attendances to insert or error.")
+                if attendances and valid:
+                    inserted = attendance_manager.bulk_insert(attendances)
+                    print(f"Inserted {len(inserted)} attendances.")
+                else:
+                    print("No attendances to insert or error.")
