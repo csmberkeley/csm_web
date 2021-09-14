500528b7ad9b759fa2924a4112ad73b544cae7c5
smartspot2 <smartspot2@gmail.com>
2021-09-13 18:19:05 -0700

    Take active into account for attendance check, update prints

--- csm_web/scheduler/management/commands/check_attendances.py
+++ csm_web/scheduler/management/commands/check_attendances.py
@@ -19,14 +19,17 @@ class Command(BaseCommand):
         print("checking attendance section enrollment...")
         sections = Section.objects.prefetch_related('sectionoccurrence_set')
         for section in sections:
-            enrolled = set(section.students.filter(active=True).values_list('id', flat=True))
+            # only get active students (these will be shown to the mentor)
+            enrolled_student_objs = section.students.filter(active=True)
+            enrolled = set(enrolled_student_objs.values_list('id', flat=True))
             for so in section.sectionoccurrence_set.filter(date__gte=start, date__lte=end).prefetch_related('attendance_set__student'):
                 # print(so.attendance_set.values_list('student').first())
                 so_students = set(so.attendance_set.values_list('student', flat=True))
                 if enrolled != so_students:
-                    print(f"Incorrect for section {section}")
-                    print(f"\tenrolled: {section.students.all()}")
-                    print(f"\tattendance ({so.date}): {Student.objects.filter(id__in=so_students)}")
+                    print(f"Incorrect for section (id {section.pk}) {section}")
+                    print(f"\tenrolled: ids {enrolled}\n\t\t{enrolled_student_objs}")
+                    print(
+                        f"\tattendance ({so.date}): ids {so_students}\n\t\t{Student.objects.filter(id__in=so_students)}")
 
     def _check_attendance_sections(self, start, end):
         print("checking section occurrence sections...")
@@ -38,7 +41,8 @@ class Command(BaseCommand):
                 # get the section occurrence for the section on the same date
                 expected_so = section.sectionoccurrence_set.get(date=att_so.date)
                 if att_so_section != student.section:
-                    print(f"Incorrect for student {student}, attendance {attendance}")
+                    print(
+                        f"Incorrect for student (id {student.pk}) {student}, attendance (id {attendance.pk}) {attendance}")
                     print(f"\tgot (id {att_so_section.pk}) {att_so_section}\n\t\texpected (id {section.pk}) {section}")
                     print(
                         f"\tSectionOccurrence (id {att_so.pk}) {att_so}\n\t\texpected (id {expected_so.pk}) {expected_so}")
