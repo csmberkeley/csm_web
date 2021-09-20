0c881657f174c70ab7779f79be42830e7aee11b6
smartspot2 <smartspot2@gmail.com>
2021-09-20 03:04:15 -0700

    Hotfix: remove postgres manager and manually create and save objects

--- csm_web/scheduler/management/commands/create_attendances.py
+++ csm_web/scheduler/management/commands/create_attendances.py
@@ -5,7 +5,7 @@ from psqlextra.util import postgres_manager
 from psqlextra.types import ConflictAction
 from datetime import timedelta
 import logging
-from django.db import transaction
+from django.db import transaction, Error
 
 
 logger = logging.getLogger(__name__)
@@ -18,35 +18,49 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         week_start, _ = week_bounds(timezone.now().date())
         sections = Section.objects.all()
-        print(f"Updating attendance for week of {week_start}")
+        logger.info(f"Updating attendance for week of {week_start}")
         with transaction.atomic():
-            with postgres_manager(SectionOccurrence) as so_db:
-                so_manager = so_db.on_conflict(["date", "section"], ConflictAction.NOTHING)
+            # with postgres_manager(SectionOccurrence) as so_db:
+            #     so_manager = so_db.on_conflict(["date", "section"], ConflictAction.NOTHING)
 
-                sos = [{"date": week_start + timedelta(days=day_to_number(spacetime.day_of_week)),
-                        "section": section} for section in sections for spacetime in section.spacetimes.all()]
-                sos = so_manager.bulk_insert(sos, return_model=True)
-                print(f"Inserted {len(sos)} SectionOccurrences.")
+            sos = [{"date": week_start + timedelta(days=day_to_number(spacetime.day_of_week)),
+                    "section": section} for section in sections for spacetime in section.spacetimes.all()]
+            sos_models = []  # list of section occurrence models
+            for so in sos:
+                try:
+                    cur = SectionOccurrence(**so)
+                    cur.save()
+                    sos_models.append(cur)
+                except Error as e:
+                    logger.error("<Logging> Failed to save {so};", e)
+            # sos = so_manager.bulk_insert(sos, return_model=True)
+            logger.info(f"Inserted {len(sos_models)} SectionOccurrences.")
 
-            with postgres_manager(Attendance) as attend_db:
-                attendance_manager = attend_db.on_conflict(["sectionOccurrence", "student"], ConflictAction.NOTHING)
-                attendances = [{"student": student, "sectionOccurrence": so, "presence": ''}
-                               for so in sos for student in so.section.students.filter(active=True)]
+            # with postgres_manager(Attendance) as attend_db:
+            #     attendance_manager = attend_db.on_conflict(["sectionOccurrence", "student"], ConflictAction.NOTHING)
+            attendances = [{"student": student, "sectionOccurrence": so, "presence": ''}
+                           for so in sos_models for student in so.section.students.filter(active=True)]
 
-                # validate attendances
-                valid = True
+            # validate attendances
+            valid = True
+            for attendance in attendances:
+                student = attendance.get("student")
+                SO = attendance.get("sectionOccurrence")
+                if student and SO:
+                    if student.section.pk != SO.section.pk:
+                        valid = False
+                        logger.error(
+                            f"<Logging> student.section.pk != SO.section.pk, attendance to add: {attendances}")
+                        break
+
+            if attendances and valid:
                 for attendance in attendances:
-                    student = attendance.get("student")
-                    SO = attendance.get("sectionOccurrence")
-                    if student and SO:
-                        if student.section.pk != SO.section.pk:
-                            valid = False
-                            logger.error(
-                                f"<Logging> student.section.pk != SO.section.pk, attendance to add: {attendances}")
-                            break
+                    try:
+                        Attendance(**attendance).save()
+                    except Error as e:
+                        logger.error("<Logging> Failed to save {attendance};", e)
+                # inserted = attendance_manager.bulk_insert(attendances)
 
-                if attendances and valid:
-                    inserted = attendance_manager.bulk_insert(attendances)
-                    print(f"Inserted {len(inserted)} attendances.")
-                else:
-                    print("No attendances to insert or error.")
+                logger.info(f"Inserted {len(attendances)} attendances.")
+            else:
+                logger.info("No attendances to insert or error.")
