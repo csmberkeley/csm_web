a524ccf4efccc0b3dcbf4c6a213dd5e7f45e6c84
vivrdprasanna <59852610+vivrdprasanna@users.noreply.github.com>
2022-03-11 18:04:13 -0800

    deleted view.py, fixed calling cascade

--- csm_web/scheduler/models.py
+++ csm_web/scheduler/models.py
@@ -287,7 +287,7 @@ class Resource(ValidatingModel):
         ordering = ['week_num']
 
 class ResourceLink(ValidatingModel):
-    resource = models.ForeignKey(Resource, on_delete = models.CASCADE())
+    resource = models.ForeignKey(Resource, on_delete = models.CASCADE)
     name = models.CharField(max_length=200)
     link_to_resource = models.URLField(max_length=255)
 
