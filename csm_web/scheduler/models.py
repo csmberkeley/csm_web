c07104300ca958217c7bbf9c7eeeacbbbcbff32e
Ryan McConnell <rsmcconnell@berkeley.edu>
2022-04-15 17:41:00 -0700

    semester 7 default test

--- csm_web/scheduler/models.py
+++ csm_web/scheduler/models.py
@@ -40,7 +40,7 @@ def default_semester():
     if Semester.objects.exists():
         return Semester.objects.all().first()
     else:
-        return Semester(1)
+        return Semester(7)
 
 
 class User(AbstractUser):
