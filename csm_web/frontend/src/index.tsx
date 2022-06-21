8f44990d9b0f2382ffd4be397d5b8fc7b9d0e19a
Alec Li <smartspot2@gmail.com>
2022-06-20 22:57:56 -0700

    Migrate profiles, courses, userinfo to react-query, refactor loading spinners

--- csm_web/frontend/src/index.tsx
+++ csm_web/frontend/src/index.tsx
@@ -2,12 +2,28 @@ import App from "./components/App";
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { ReactQueryDevtools } from "react-query/devtools";
+
+// react-query setup
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // 5 minute stale time
+      staleTime: 1000 * 60 * 5
+    }
+  }
+});
 
 const root = createRoot(document.getElementById("app") as HTMLElement);
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>
 );
