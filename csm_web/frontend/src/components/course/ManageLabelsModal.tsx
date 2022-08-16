e9c961716eccf7a606db605627fe79402c58764e
Austen Liao <austenliao@berkeley.edu>
2022-08-15 20:36:24 -0700

    post 8/15 #2, worked on CSS + more

--- csm_web/frontend/src/components/course/ManageLabelsModal.tsx
+++ csm_web/frontend/src/components/course/ManageLabelsModal.tsx
@@ -29,6 +29,8 @@ export const ManageLabelsModal = ({
    */
   const [courseLabels, setCourseLabels] = useState<Label[]>([]);
 
+  const [newLabelKey, setNewLabelKey] = useState(-1);
+
   // fetch all labels upon first mount
   useEffect(() => {
     fetchJSON(`/courses/${courseId}/labels`).then(data => {
@@ -53,8 +55,9 @@ export const ManageLabelsModal = ({
     const course = courseLabels[0].course;
     setCourseLabels([
       ...courseLabels,
-      { id: -1, course: course, sections: [], name: "", description: "", showPopup: false }
+      { id: newLabelKey, course: course, sections: [], name: "", description: "", showPopup: false }
     ]);
+    setNewLabelKey(newLabelKey - 1);
   };
 
   function handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
@@ -71,7 +74,8 @@ export const ManageLabelsModal = ({
           <div className="data-export-modal-header">List of labels for {title}</div>
 
           {/* // starting off by wrapping everything in a flexbox div with classname managelabelsmodal & playing around with that -Yahya  */}
-          <div className="manage-Labels-Modal">
+          {/* this is the entire table for the flexbox */}
+          <div className="manage-labels-modal">
             <ul>
               {courseLabels.map((label: Label) => (
                 <EditLabelRow
@@ -84,11 +88,12 @@ export const ManageLabelsModal = ({
                 />
               ))}
             </ul>
-            <span onClick={handleNewRow}>Add a new label</span>
           </div>
-          <input type="submit" value="Save" />
+          {/* end manageLabelModals div */}
+
+          <span onClick={handleNewRow}>Add a new label</span>
         </div>
-        {/* end manageLabelModals div */}
+        <input type="submit" value="Save" />
       </form>
     </Modal>
   );
@@ -129,34 +134,50 @@ export const EditLabelRow = ({ label, onChangeRow, removeLabel }: EditLabelRowPr
     <div>
       {/* // if not editing labels.. */}
       {!editingLabels && (
+        // begining of evenly spaced row
         <div className="matcher-assignment-section-times-edit">
-          {label.name}: {label.description}
+          {label.name === "" && <div>Name of label</div>}
+          {label.name !== "" && <div>{label.name}</div>}
+          {label.description === "" && <div>Description of label</div>}
+          {label.description !== "" && <div>{label.description}</div>}
           <label>
             <Toggle id={label.name} defaultChecked={label.showPopup} disabled={true} />
           </label>
           <Pencil className="icon matcher-assignment-section-times-edit-icon" onClick={toggleEditingLabels} />
           <Trash className="icon matcher-assignment-section-times-edit-icon" onClick={removeLabel} />
         </div>
+        // end of evenly spaced row
       )}
       {/* // if editing labels... */}
       {editingLabels && (
-        <span>
-          <input
-            name="name"
-            defaultValue={label.name}
-            placeholder={label.name === "" ? "Name of label" : label.name}
-            onChange={handleChange}
-          />
-          <input
-            name="description"
-            defaultValue={label.description}
-            placeholder={label.description === "" ? "Description of label" : label.description}
-            onChange={handleChange}
-          />
-          <Toggle defaultChecked={label.showPopup} onChange={handleChange} />
-          <Exit className="icon matcher-assignment-section-times-edit-icon" onClick={toggleEditingLabels} />
-        </span>
+        <div>
+          <span>
+            <input
+              name="name"
+              defaultValue={label.name}
+              placeholder={label.name === "" ? "Name of label" : label.name}
+              onChange={handleChange}
+            />
+            <input
+              name="description"
+              defaultValue={label.description}
+              placeholder={label.description === "" ? "Description of label" : label.description}
+              onChange={handleChange}
+            />
+            <Toggle defaultChecked={label.showPopup} onChange={handleChange} />
+            <Exit className="icon matcher-assignment-section-times-edit-icon" onClick={toggleEditingLabels} />
+          </span>
+        </div>
       )}
     </div>
   );
 };
+
+// 167 to 184 is editting labels div
+// 137 to 163 is not edditing label
+
+// columns
+// name
+// description
+// checbox
+// pencil / trash
