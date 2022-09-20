133c2ddadb113794a4ad8b959c66e74273982549
Sebastian Zhao <sebbyzhao@berkeley.edu>
2022-09-19 17:58:23 -0700

    Started fixes to factory.py for testing

--- csm_web/scheduler/serializers.py
+++ csm_web/scheduler/serializers.py
@@ -226,7 +226,7 @@ class ResourceSerializer(serializers.ModelSerializer):
 class SectionOccurrenceSerializer(serializers.ModelSerializer):
     attendances = AttendanceSerializer(source='attendance_set', many=True)
     section = SectionSerializer()
-    section_occurrence = SectionOccurrenceSerializer()
+    # section_occurrence = SectionOccurrenceSerializer()
     
     class Meta:
         model = SectionOccurrence
