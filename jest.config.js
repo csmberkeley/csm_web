e61609a004d126de84c1156ed8e09b685af06c2e
Alec Li <smartspot2@gmail.com>
2022-11-30 05:03:26 -0800

    Remove jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -43,14 +43,14 @@ module.exports = {
   // ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80
+  //   }
+  // },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
