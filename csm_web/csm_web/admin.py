4f4e281030ae1433a98f41a41142171bb2337a2e
Alec Li <smartspot2@gmail.com>
2022-12-26 16:08:05 -0800

    Migrate to poetry, update dependencies, add pre-commit
    Migrate to poetry for dependency management
    Setup pre-commit hooks
    Fix django settings in upgrading to 4.x
    Add __init__ files as necessary to keep module structure
    Remove old test cases, reformat edited files

--- csm_web/csm_web/admin.py
+++ csm_web/csm_web/admin.py
@@ -1,25 +1,27 @@
-from django.contrib.admin import AdminSite as DefaultAdminSite
-from django.views.decorators.cache import never_cache
 from django.conf import settings
+from django.contrib.admin import AdminSite as DefaultAdminSite
 from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
 
 
 class AdminSite(DefaultAdminSite):
-    site_header = f"{'Development ' if settings.DJANGO_ENV != settings.PRODUCTION else ''}Scheduler Admin"
+    site_header = (
+        f"{'Development ' if settings.DJANGO_ENV != settings.PRODUCTION else ''}Scheduler"
+        " Admin"
+    )
     site_title = "CSM Scheduler Admin"
     index_title = ""
 
-    @never_cache
+    @method_decorator(never_cache)
     def login(self, request, extra_context=None):
         if settings.DJANGO_ENV not in (settings.DEVELOPMENT, settings.STAGING):
-            """
-            Disable Django's built-in admin login page outside of development
-            because it's a security liability
-            """
-            return redirect('/')
+            # Disable Django's built-in admin login page outside of development
+            # because it's a security liability
+            return redirect("/")
         return super().login(request, extra_context)
 
     def each_context(self, request):
         context = super().each_context(request)
-        context['DJANGO_ENV'] = settings.DJANGO_ENV
+        context["DJANGO_ENV"] = settings.DJANGO_ENV
         return context
