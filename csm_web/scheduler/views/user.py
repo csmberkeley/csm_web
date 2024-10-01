0350cf867aa47e53c8b5b0fbb2be088819472545
ElaineShu0312 <elaineshu0755@gmail.com>
2024-09-30 17:37:13 -0700

    hopefully resolved one of the merge errors

--- csm_web/scheduler/views/user.py
+++ csm_web/scheduler/views/user.py
@@ -128,7 +128,7 @@ def user_update(request, pk):
     serializer = UserSerializer(user, data=request.data, partial=True)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
