6efcc7c5bfe17fb8262d82520a02afc6f7312923
Faith Dennis <32501997+faithdennis@users.noreply.github.com>
2024-10-28 12:13:28 -0700

    convert viewset, remove add from waitlist from views (#498)

--- csm_web/scheduler/views/waitlistedStudent.py
+++ csm_web/scheduler/views/waitlistedStudent.py
@@ -2,17 +2,15 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
+from ..models import (
+    Section, 
+    WaitlistedStudent
+)
 
-from ..models import Section, WaitlistedStudent
-from ..views.section import SectionViewSet
-from .utils import log_str, logger
+from ..serializers import WaitlistedStudentSerializer
+from .utils import get_object_or_error, logger
 
-
-@api_view(["GET"])
-def get(request, pk=None):
-    print(request)
-    return Response("Hi")
-
+DEFAULT_CAPACITY = 3
 
 @api_view(["POST"])
 def add(request, pk=None):
@@ -24,114 +22,64 @@ def add(request, pk=None):
     - if user cannot enroll in section, deny permission
     - if section is not full, enroll instead.
     """
-    section = Section.objects.get(pk=pk)
+    section = get_object_or_error(Section.objects, pk=pk)
     if not request.user.can_enroll_in_course(section.mentor.course):
-        logger.warning(
-            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for"
-            " Section %s because they are already involved in this course",
-            log_str(request.user),
-            log_str(section),
-        )
+        logger.info(
+            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for \
+                Section %s because they are already involved in this course",
+            request.user, section
+            )
         raise PermissionDenied(
-            "You are already either mentoring for this course or enrolled in a"
-            " section, or the course is closed for enrollment",
+            "You are already either mentoring for this course or enrolled in a \
+                section, or the course is closed for enrollment",
             status.HTTP_422_UNPROCESSABLE_ENTITY,
         )
-    if section.current_student_count < section.capacity:
-        sectionviewset = SectionViewSet()
-        return sectionviewset._student_add(request, section)
 
-    if section.current_waitlistedstudent_count >= section.waitlist_capacity:
+    if section.current_waitlist_count >= section.waitlist_capacity:
         logger.warning(
-            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for"
-            " Section %s because it was full",
-            log_str(request.user),
-            log_str(section),
+            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for \
+                Section %s because it was full", request.user, section,
         )
         raise PermissionDenied(
             "There is no space available in this section", status.HTTP_423_LOCKED
         )
 
-    waitliststudent_queryset = request.user.waitlistedstudent_set.filter(
+    waitlisted_student_queryset = request.user.waitlistedstudent_set.filter(
         active=True, course=section.mentor.course, section=section.id
     )
 
-    waitliststudent_queryset_all = request.user.waitlistedstudent_set.filter(
+    waitlisted_student_queryset_all = request.user.waitlistedstudent_set.filter(
         active=True, course=section.mentor.course
     )
 
-    if waitliststudent_queryset.count() == 1:
+    if waitlisted_student_queryset.count() == 1:
         logger.warning(
-            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for"
-            " Section %s because user is already enrolled in the waitlist for this"
-            " section",
-            log_str(request.user),
-            log_str(section),
+            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for \
+                Section %s because user is already enrolled in the waitlist for this"
+            " section", request.user, section,
         )
         raise PermissionDenied(
             "You are already waitlisted in this section", status.HTTP_423_LOCKED
-        )
 
-    if waitliststudent_queryset_all.count() >= section.mentor.course.max_waitlist:
+    if waitlisted_student_queryset_all.count() >= section.mentor.course.max_waitlist:
         logger.warning(
-            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for"
-            " Section %s because user is already enrolled in more than %s waitlist"
-            " sections",
-            log_str(request.user),
-            log_str(section),
-            log_str(section.mentor.course.max_waitlist),
+            "<Enrollment:Failure> User %s was unable to enroll in Waitlist for \
+                Section %s because user is already enrolled in more than %s waitlist \
+                sections", request.user, section, section.mentor.course.max_waitlist
         )
         raise PermissionDenied(
             "You are waitlisted in too many sections", status.HTTP_423_LOCKED
         )
 
-    waitlistedstudent = WaitlistedStudent.objects.create(
+    waitlisted_student = WaitlistedStudent.objects.create(
         user=request.user, section=section, course=section.mentor.course
     )
 
-    waitlistedstudent.save()
     logger.info(
         "<Enrollment:Success> User %s enrolled into Waitlist for Section %s",
-        log_str(request.user),
-        log_str(section),
+        request.user, section
     )
     return Response(status=status.HTTP_201_CREATED)
-
-
-# @api_view(["PATCH"])
-# def add_from_waitlist(request, pk=None):
-#     """
-#     Endpoint: /api/waitlistedstudent/<pk>/add_from_waitlist
-
-#     PATCH: Add a student to a section off the waitlist. Will remove from all
-#     other waitlists as well
-#     - waitlist student is deactivated
-#     - changes nothing if fails to add class
-
-#     """
-#     waitlisted_student = WaitlistedStudent.objects.get(pk=pk)
-#     # TODOLATER make sure that whoever calls this function has the rights to add
-#     # TODOLATER maybe move this to section so that do not need to add student from here
-#     sectionviewset = SectionViewSet()
-#     request.user = waitlisted_student.user
-#     add_response = sectionviewset._student_add(request, waitlisted_student.section)
-#     if add_response.status_code != 201:
-#         return add_response
-
-#     waitlist_set = WaitlistedStudent.objects.filter(
-#         user=waitlisted_student.user, active=True, course=waitlisted_student.course
-#     )
-#     for waitlist in waitlist_set:
-#         waitlist.active = False
-#         waitlist.save()
-
-#     logger.info(
-#         "<Enrollment:Success> User %s removed from all Waitlists for Course %s",
-#         log_str(waitlisted_student.user),
-#         log_str(waitlisted_student.course),
-#     )
-#     return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 @api_view(["PATCH"])
 def drop(request, pk=None):
@@ -155,5 +103,5 @@ def drop(request, pk=None):
     waitlisted_student.active = False
 
     waitlisted_student.save()
-    logger.info("<Drop> User %s Waitlisted Section", log_str(request.user))
+    logger.info("<Drop> User %s dropped from Waitlist for Section %s", request.user, waitlisted_student.section)
     return Response(status=status.HTTP_204_NO_CONTENT)
