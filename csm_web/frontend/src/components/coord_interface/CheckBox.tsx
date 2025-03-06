9044ac4e1247980085c747195c1d85e96eca090c
Gabriel Han <gabrielhan@berkeley.edu>
2025-03-06 03:57:53 -0800

    front end table searching and select data logic

--- csm_web/frontend/src/components/coord_interface/CheckBox.tsx
+++ csm_web/frontend/src/components/coord_interface/CheckBox.tsx
@@ -8,22 +8,24 @@ interface CheckBoxProps {
 
 export function CheckBox({ id, onClick: onClick }: CheckBoxProps) {
   return (
-    <td className="checkbox">
-      <div className="checkbox-wrapper">
-        <input className="inp-cbx" id={id + "check"} type="checkbox" onClick={onClick} />
-        <label className="cbx" htmlFor={id + "check"}>
-          <span>
-            <svg width="12px" height="10px">
-              <use xlinkHref="#check"></use>
-            </svg>
-          </span>
-        </label>
-        <svg className="inline-svg">
-          <symbol id="check" viewBox="0 0 12 10">
-            <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
-          </symbol>
-        </svg>
-      </div>
-    </td>
+    <div className={styles}>
+      <td className="checkbox">
+        <div className="checkbox-wrapper">
+          <input className="inp-cbx" id={id + "check"} type="checkbox" onClick={onClick} />
+          <label className="cbx" htmlFor={id + "check"}>
+            <span>
+              <svg width="12px" height="10px">
+                <use xlinkHref="#check"></use>
+              </svg>
+            </span>
+          </label>
+          <svg className="inline-svg">
+            <symbol id="check" viewBox="0 0 12 10">
+              <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
+            </symbol>
+          </svg>
+        </div>
+      </td>
+    </div>
   );
 }
