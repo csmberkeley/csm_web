b3d8a21f7250bcc5b7a1809f10f72615f35c89a4
Alec Li <smartspot2@gmail.com>
2025-03-10 17:15:14 -0700

    Add calendar to mentor attendance view (#475)
    * Add calendar to mentor attendance view
    
    * Fix timezone bug when comparing dates in the mentor attendance calendar
    
    * Default to most recent past occurrence instead of most recent future occurrence
    
    * Fix initial attendance bug, add icon for unmarked attendances
    
    * Update cypress tests for changes in initial date selection, update student wotd date selection

--- cypress/e2e/section/student-section.cy.ts
+++ cypress/e2e/section/student-section.cy.ts
@@ -14,7 +14,7 @@ describe("word of the day", () => {
     cy.visit("/sections/1/attendance");
     cy.wait("@get-attendances");
 
-    // get both dates
+    // get both dates; table is sorted with most recent first, so dates[0] > dates[1]
     const dates = [];
     cy.get("#attendance-table tbody tr")
       .should("have.length", 2)
@@ -29,16 +29,33 @@ describe("word of the day", () => {
           .then(text => dates.push(text));
       });
 
+    // closure around access to ensure that the evaluation occurs asynchronously
+    const PAST_DATE = () => dates[1];
+    const FUTURE_DATE = () => dates[0];
+
     // ensure word of the day is visible
     cy.get("#word-of-the-day-card").within(() => {
       cy.contains(".word-of-the-day-title", /submit word of the day/i).should("be.visible");
 
-      // first date (default) should not have word of the day, second date should
-      cy.get(".form-select[name='word-of-the-day-date']").find(":selected").invoke("text").should("eq", dates[0]);
+      // initial date selection should be the most recent past date, which should have a word of the day
+      cy.get(".form-select[name='word-of-the-day-date']").find(":selected").invoke("text").should("eq", PAST_DATE());
+      cy.get(".form-select[name='word-of-the-day-date']")
+        .find(":not(:selected)")
+        .invoke("text")
+        .should("eq", FUTURE_DATE());
+      // submit button should be disabled initially (no word has been inputted)
       cy.contains(".primary-btn", /submit/i).should("be.disabled");
-      cy.get(".word-of-the-day-deadline").should("not.exist"); // no deadline set
+      // no deadline set
+      cy.get(".word-of-the-day-deadline").should("not.exist");
 
-      // word of the day for second date; should fail
+      // swap to future date
+      cy.get(".form-select[name='word-of-the-day-date']").select(FUTURE_DATE());
+      // submit button should still be disabled (no word has been inputted)
+      cy.contains(".primary-btn", /submit/i).should("be.disabled");
+      // no deadline set
+      cy.get(".word-of-the-day-deadline").should("not.exist");
+
+      // word of the day for future date; should fail
       cy.get(".form-input[name='word-of-the-day']").type("wordoftheday");
       cy.contains(".primary-btn", /submit/i)
         .should("not.be.disabled")
@@ -51,10 +68,11 @@ describe("word of the day", () => {
         .invoke("text")
         .should("match", /incorrect word of the day/i);
 
-      // swap to second date
-      cy.get(".form-select[name='word-of-the-day-date']").select(dates[1]);
-
-      cy.get(".word-of-the-day-deadline").should("not.exist"); // no deadline set
+      // swap to past date
+      cy.get(".form-select[name='word-of-the-day-date']").select(PAST_DATE());
+      // no deadline set
+      cy.get(".word-of-the-day-deadline").should("not.exist");
+      // text should still be here from the last submission
       cy.contains(".primary-btn", /submit/i).click();
 
       cy.wait("@put-wotd").its("response.statusCode").should("eq", 200);
@@ -162,23 +180,26 @@ describe("word of the day", () => {
           .then(text => dates.push(text));
       });
 
+    // closure around access to ensure that the evaluation occurs asynchronously
+    const PAST_DATE = () => dates[1];
+    const FUTURE_DATE = () => dates[0];
+
     cy.get("#word-of-the-day-card").within(() => {
       cy.contains(".word-of-the-day-title", /submit word of the day/i).should("be.visible");
 
-      // currently should be on future date; deadline not passed yet
-      cy.get(".form-select[name='word-of-the-day-date']").find(":selected").invoke("text").should("eq", dates[0]);
-      cy.get(".word-of-the-day-deadline").should("be.visible").should("not.have.class", "passed");
-
-      // switch to past date; deadline should have passed
-      cy.get(".form-select[name='word-of-the-day-date']").select(dates[1]);
+      // currently should be on past date; deadline should have passed
+      cy.get(".form-select[name='word-of-the-day-date']").find(":selected").invoke("text").should("eq", PAST_DATE());
       cy.get(".word-of-the-day-deadline").should("be.visible").should("have.class", "passed");
 
-      // try typing something now; should not allow submitting
+      // try typing something; should not allow submitting
       cy.get(".form-input[name='word-of-the-day']").type("wordoftheday");
       cy.contains(".primary-btn", /submit/i).should("be.disabled");
 
-      // switch to future date; should be able to submit normally
-      cy.get(".form-select[name='word-of-the-day-date']").select(dates[0]);
+      // switch to future date; deadline not passed yet
+      cy.get(".form-select[name='word-of-the-day-date']").select(FUTURE_DATE());
+      cy.get(".word-of-the-day-deadline").should("be.visible").should("not.have.class", "passed");
+
+      // should be able to submit normally
       cy.get(".form-input[name='word-of-the-day']").clear().type("password");
       cy.contains(".primary-btn", /submit/i)
         .should("not.be.disabled")
