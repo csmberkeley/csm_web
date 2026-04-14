453b609bcd9f2b95bc435995b7c4498b0c9d2b24
Ben Hung <20ben16@gmail.com>
2026-04-13 17:23:45 -0700

    Added refresh method for Google API token

--- csm_web/scheduler/notifications/test.py
+++ csm_web/scheduler/notifications/test.py
@@ -8,19 +8,48 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import requests
+import time
+import json
+import datetime
 
 # Scopes define what permissions you're requesting
 SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
+
+creds_data = {}
+with open('credentials.json', 'r') as f:
+    creds_data = json.load(f)
+creds_data = creds_data["installed"]
+
+def get_access_token(client_id: str, client_secret: str, refresh_token: str) -> tuple[str, int]:
+
+    response = requests.post("https://oauth2.googleapis.com/token", data={
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token,
+        "client_id": client_id,
+        "client_secret": client_secret,
+    })
+    response.raise_for_status()
+    data = response.json()
+
+    return data["access_token"], data["expires_in"]
 
 def get_credentials():
     creds = None
 
     if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)    
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+
+            creds.token, expires_in = get_access_token(
+                client_id=creds_data["client_id"],
+                client_secret=creds_data["client_secret"],
+                refresh_token=creds.refresh_token,
+            )
+            creds.expiry = datetime.datetime.utcnow() + datetime.timedelta(seconds=expires_in)
+            # creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
@@ -48,7 +77,7 @@ def gmail_send():
 
     message.set_content("This is automated draft mail")
 
-    message["To"] = "alex05sim@berkeley.edu"
+    message["To"] = "benhung@berkeley.edu"
     message["From"] = "mentors@berkeley.edu"
     message["Subject"] = "Hi Alex Sim"
 
